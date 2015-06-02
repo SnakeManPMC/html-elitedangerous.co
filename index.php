174fbdb4e098d4e8478fda9dbc68839821d42fe5
Snake Man <snakeman@phnet.fi>
2015-06-03 00:57:52 +0300

    css fix for the web site main LAYOUT shit.

--- index.php
+++ index.php
@@ -19,51 +19,66 @@ google analytics here
 google ads here
 paypal here
 -->
+
 <header>
 <h1><b>PMC Elite Dangerous</b></h1>
 </header>
 
-<p>Welcome to PMC Tactical's Elite Dangerous pages. We are the old school PMC Space Monkey's from 1984 :)</p>
-
 <section>
 
+<p>Welcome to PMC Tactical's Elite Dangerous pages. We are the old school PMC Space Monkey's from 1984 :)</p>
+
+<div id="icat">
 <h2>Combat</h2>
-<a href="bounty-hunting.php">Bounty Hunting</a> <br><br>
-<a href="conflict-zones.php">Conflict Zones</a> <br><br>
-<a href="resource-extraction-sites.php">Resource Extraction Sites (RES)</a> <br><br>
-<a href="weapons.php">Weapons</a> <br><br>
+<a href="bounty-hunting.php">Bounty Hunting</a><br><br>
+<a href="conflict-zones.php">Conflict Zones</a><br><br>
+<a href="resource-extraction-sites.php">Resource Extraction Sites (RES)</a><br><br>
+<a href="weapons.php">Weapons</a>
+</div>
 
+<div id="icat">
 <h2>Trading</h2>
-<a href="commodities-market.php">Commodities Market</a> <br><br>
-<a href="eddb.php">EDDB</a> <br><br>
-<a href="eddn.php">EDDN</a> <br><br>
-<a href="eliteocr.php">EliteOCR</a> <br><br>
+<a href="commodities-market.php">Commodities Market</a><br><br>
+<a href="eddb.php">EDDB</a><br><br>
+<a href="eddn.php">EDDN</a><br><br>
+<a href="eliteocr.php">EliteOCR</a><br><br>
 <a href="tradedangerous.php">TradeDangerous</a> by kfsone<br><br>
-<a href="landing-pad-size-distance-from-star-tutorial.php">Landing pad size and distance from star tutorial</a> <br><br>
+<a href="landing-pad-size-distance-from-star-tutorial.php">Landing pad size and distance from star tutorial</a>
+</div>
 
+<div id="icat">
 <h2>Exploration</h2>
-<a href="exploration.php">Exploration</a> <br><br>
-<a href="system-map.php">System Map Guide</a> <br><br>
+<a href="exploration.php">Exploration</a><br><br>
+<a href="system-map.php">System Map Guide</a><br><br>
+<a href="hyperspace-jump.php">Hyperspace Jump</a><br><br>
+<a href="super-cruise.php">Super Cruise</a><br><br>
+<a href="star-systems.php">Star Systems</a>
+</div>
 
+<div id="icat">
 <h2>Game Releases</h2>
-<a href="beta-2.php">Beta 2 notes</a> <br><br>
-<a href="beta-3.php">Beta 3 notes</a> <br><br>
-<a href="final-release.php">Final Release</a> <br><br>
-<a href="gamma.php">Gamma (Release Candidate)</a> <br><br>
-<a href="history.php">History</a> <br><br>
-<a href="news.php">News or some snippets</a> <br><br>
+<a href="beta-2.php">Beta 2 notes</a><br><br>
+<a href="beta-3.php">Beta 3 notes</a><br><br>
+<a href="final-release.php">Final Release</a><br><br>
+<a href="gamma.php">Gamma (Release Candidate)</a><br><br>
+<a href="history.php">History</a><br><br>
+<a href="news.php">News or some snippets</a>
+</div>
 
+<div id="icat">
 <h2>Miscellaneous</h2>
-<a href="cmdr-snake-man-my-path-to-a-triple-elite.php">Cmdr Snake Man, my path to a Triple Elite</a> <br><br>
-<a href="pmc-galaxy-loop.php">PMC Galaxy Loop</a> <br><br>
-<a href="controls.php">Controls (keyboard etc)</a> <br><br>
-<a href="hyperspace-jump.php">Hyperspace Jump</a> <br><br>
-<a href="mining.php">Mining</a> <br><br>
-<a href="ships.php">Ships</a> <br><br>
-<a href="space-stations.php">Space Stations</a> <br><br>
-<a href="star-systems.php">Star Systems</a> <br><br>
-<a href="super-cruise.php">Super Cruise</a> <br><br>
-<a href="my-path-to-anaconda.php">My path to Anaconda</a> <br><br>
+<a href="controls.php">Controls (keyboard etc)</a><br><br>
+<a href="mining.php">Mining</a><br><br>
+<a href="ships.php">Ships</a><br><br>
+<a href="space-stations.php">Space Stations</a>
+</div>
+
+<div id="icat">
+<h2>PMC Cmdr's Log</h2>
+<a href="cmdr-snake-man-my-path-to-a-triple-elite.php">Cmdr Snake Man, my path to a Triple Elite</a><br><br>
+<a href="pmc-galaxy-loop.php">PMC Galaxy Loop</a><br><br>
+<a href="my-path-to-anaconda.php">My path to Anaconda</a>
+</div>
 
 </section>
 
