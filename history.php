fc6ec798f8061198e9b446c351fcaa892bdd596b
Snake Man <snakeman@phnet.fi>
2015-06-09 19:47:08 +0300

    History update.

--- history.php
+++ history.php
@@ -23,6 +23,7 @@
 <h2>History of Elite Dangerous</h2>
 
 <p>
+Tuesday. Jun 9th, 2015. v1.3.01<br>
 Monday. Jun 8th, 2015. v1.3 Server side update<br>
 Friday. Jun 5th, 2015. v1.3<br>
 Thursday. Jun 4th, 2015. v1.3 beta 8<br>
@@ -119,6 +120,9 @@ Friday. Dec 20th, 2013. Alpha Phase One
 </p>
 <h2>Detailed major release times (UTC + 2hrs or 3hrs daylight lameness time):</h2>
 <p>
+v1.3.01<br>
+1903hrs Release
+<br><br>
 v1.3<br>
 1658hrs Forum announcement<br>
 1736hrs Servers down<br>
