abb6e5475c0ad46c7ac038cbf1aa17cc92987bdd
Snake Man <snakeman@phnet.fi>
2015-10-05 01:45:33 +0300

    History ordered in sensible way.

--- history.php
+++ history.php
@@ -23,266 +23,163 @@
 <h2>History of Elite Dangerous</h2>
 
 <p>
-Monday. Sep 1st, 2015. v1.4 beta<br>
-Wednesday. Aug 5th, 2015. v1.3.08<br>
-Thursday. Jul 7th, 2015. v1.3.07<br>
-Friday. Jun 26th, 2015. v1.3.06 Server side update<br>
-Thursday. Jun 25th, 2015. v1.3.06<br>
-Friday. Jun 19th, 2015. v1.3.05 Server side update<br>
-Thursday. Jun 18th, 2015. v1.3.05<br>
-Tuesday. Jun 16th, 2015. v1.3.04<br>
-Friday. Jun 12th, 2015. v1.3.03<br>
-Thursday. Jun 11th, 2015. v1.3.02 Server side update<br>
-Wednesday. Jun 10th, 2015. v1.3.02<br>
-Tuesday. Jun 9th, 2015. v1.3.01<br>
-Monday. Jun 8th, 2015. v1.3 Server side update<br>
-Friday. Jun 5th, 2015. v1.3<br>
-Thursday. Jun 4th, 2015. v1.3 beta 8<br>
-Wednesday. Jun 3rd, 2015. v1.3 beta 7<br>
-Tuesday. Jun 2nd, 2015. v1.3 beta 6<br>
-Monday. Jun 1st, 2015. v1.3 beta 5 ?<br>
-Friday. May 29th, 2015. v1.3 beta 4<br>
-Thursday. May 28th, 2015. v1.3 beta 3<br>
-?. May ?th, 2015. v1.3 beta 2<br>
-Tuesday. May 26th, 2015. v1.3 beta<br>
-Friday. Apr 10th, 2015. v1.2.07<br>
-Wednesday. Mar 25th, 2015. v1.2.06<br>
-Wednesday. Mar 18th, 2015. v1.2.05<br>
-Friday. Mar 13th, 2015. v1.2.04<br>
-Thursday. Mar 12th, 2015. v1.2.03<br>
-Wednesday. Mar 11th, 2015. v1.2.02 hotfix<br>
-Wednesday. Mar 11th, 2015. v1.2.01<br>
-Tuesday. Mar 10th, 2015. v1.2<br>
-Monday. Mar 9th, 2015. v1.2 beta 4<br>
-Friday. Mar 6th, 2015. v1.2 beta 3<br>
-Thursday. Mar 5th, 2015. v1.1.06<br>
-Wednesday. Mar 4th, 2015. v1.2 beta 2<br>
-Tuesday. Mar 3rd, 2015. v1.2 beta 1<br>
-Thursday. Feb 19th, 2015. v1.1.05<br>
-Friday. Feb 13th, 2015. v1.1.04<br>
-Thursday. Feb 12th, 2015. v1.1.03<br>
-Wednesday. Feb 11th, 2015. v1.1.02<br>
-Tuesday. Feb 10th, 2015. v1.1.01<br>
-Tuesday. Feb 10th, 2015. v1.1 Release<br>
-Friday. Feb 6th, 2015. v1.1 Beta 3<br>
-Thursday. Feb 5th, 2015. v1.1 Beta 2<br>
-Tuesday. Feb 3rd, 2015. v1.1 Beta &lt;- for alpha/beta backers only<br>
-Wednesday. Jan 28th, 2015. v1.07<br>
-Thursday. Jan 22nd, 2015. v1.06<br>
-Thursday. Jan 15th, 2015. v1.05<br>
-Thursday. Jan 8th, 2015. v1.04<br>
-Wednesday. Dec 24th, 2014. Server Maintenance (no fixes changelog were posted)<br>
-Monday. Dec 22nd, 2014. Server Update (trading market fixes)<br>
-Friday. Dec 19th, 2014. v1.03<br>
-Thursday. Dec 18th, 2014. v1.02<br>
-Wednesday. Dec 17th, 2014. v1.01<br>
-<b>Tuesday. Dec 16th, 2014. Elite Dangerous v1.0.0.0 !</b><br>
-Monday. Dec 15th, 2014. Gamma 2.07<br>
-Monday. Dec 15th, 2014. Gamma 2.06<br>
-Sunday. Dec 14th, 2014. Gamma 2.05 hotfix build<br>
-Sunday. Dec 14th, 2014. Gamma 2.05<br>
-Saturday. Dec 13th, 2014. Gamma 2.04<br>
-Friday. Dec 12th, 2014. Gamma 2.03<br>
-Thursday. Dec 11th, 2014. Gamma 2.02<br>
-Thursday. Dec 11th, 2014. Gamma 2.01<br>
-Wednesday. Dec 10th, 2014. Gamma 2.0<br>
-Tuesday. Dec 2nd, 2014. Gamma 1.05<br>
-Friday. Nov 28th, 2014. Gamma 1.04<br>
-Thursday. Nov 27th, 2014. Gamma 1.03<br>
-Wednesday. Nov 26th, 2014. Gamma 1.02<br>
-Monday. Nov 24th, 2014. Gamma 1.01<br>
-Saturday. Nov 22nd, 2014. Gamma 1.0<br>
-Friday. Nov 21st, 2014. Beta 3.9.1<br>
-Friday. Nov 21st, 2014. Beta 3.9<br>
-Tuesday. Nov 11th, 2014. Beta 3.05<br>
-Thursday. Nov 6th, 2014. Beta 3.04<br>
-Friday. Oct 31st, 2014. Beta 3.03<br>
-Thursday. Oct 30th, 2014. Beta 3.02<br>
-Thursday. Oct 30th, 2014. Beta 3.01<br>
-Wednesday. Oct 29th, 2014. Beta 3.00<br>
-Tuesday. Oct 14th, 2014. Beta 2.06<br>
-Thursday. Oct 9th, 2014. Beta 2.05<br>
-Friday. Oct 3rd, 2014. Beta 2.04<br>
-Thursday. Oct 2nd, 2014. Beta 2.03<br>
-Wednesday. Oct 1st, 2014. Beta 2.02<br>
-Wednesday. Oct 1st, 2014. Beta 2.01<br>
-Wednesday. Oct 1st, 2014. Beta 2.00<br>
-Tuesday. Sep 16th, 2014. Beta 1.06<br>
-Tuesday. Sep 16th, 2014. Beta 1.05<br>
-Wednesday. Sep 3rd, 2014. Beta 1.04<br>
-Friday. Aug 1st, 2014. Beta 1.03<br>
-Thursday. Jul 31st, 2014. Beta 1.02<br>
-Wednesday. Jul 30th, 2014. Beta 1.01<br>
-Wednesday. Jul 30th, 2014. Beta 1.00<br>
-Friday. Jun 27th, 2014. Premium Beta 2.02<br>
-Thursday. Jun 26th, 2014. Premium Beta 2.01<br>
-Tuesday. Jun 24th, 2014. Premium Beta 2.00<br>
-Friday. May 30th, 2014. Premium Beta<br>
-Tuesday. May 20th, 2014. Alpha 4.03<br>
-Tuesday. May 20th, 2014. Alpha 4.02<br>
-Friday. May 16th, 2014. Alpha 4.01<br>
-Thursday. May 15th, 2014. Alpha 4.00<br>
-Tuesday. Apr 1st, 2014. Alpha 3.4<br>
-Monday. Mar 17th, 2014. Alpha 3.1 - Docking and Outfitting<br>
-Thrusday. Mar 13th, 2014. Alpha 3.00 - Docking and Outfitting<br>
-Wednesday. Feb 5th, 2014. Alpha 2.00 - Multiplayer<br>
+Friday. Dec 20th, 2013. Alpha Phase One<br>
 Wednesday. Jan 22nd, 2014. Multiplayer Combat<br>
-Friday. Dec 20th, 2013. Alpha Phase One
+Wednesday. Feb 5th, 2014. Alpha 2.00 - Multiplayer<br>
+Thrusday. Mar 13th, 2014. Alpha 3.00 - Docking and Outfitting<br>
+Monday. Mar 17th, 2014. Alpha 3.1 - Docking and Outfitting<br>
+Tuesday. Apr 1st, 2014. Alpha 3.4<br>
+Thursday. May 15th, 2014. Alpha 4.00<br>
+Friday. May 16th, 2014. Alpha 4.01<br>
+Tuesday. May 20th, 2014. Alpha 4.02<br>
+Tuesday. May 20th, 2014. Alpha 4.03<br>
+Friday. May 30th, 2014. Premium Beta<br>
+Tuesday. Jun 24th, 2014. Premium Beta 2.00<br>
+Thursday. Jun 26th, 2014. Premium Beta 2.01<br>
+Friday. Jun 27th, 2014. Premium Beta 2.02<br>
+Wednesday. Jul 30th, 2014. Beta 1.00<br>
+Wednesday. Jul 30th, 2014. Beta 1.01<br>
+Thursday. Jul 31st, 2014. Beta 1.02<br>
+Friday. Aug 1st, 2014. Beta 1.03<br>
+Wednesday. Sep 3rd, 2014. Beta 1.04<br>
+Tuesday. Sep 16th, 2014. Beta 1.05<br>
+Tuesday. Sep 16th, 2014. Beta 1.06<br>
+Wednesday. Oct 1st, 2014. Beta 2.00<br>
+Wednesday. Oct 1st, 2014. Beta 2.01<br>
+Wednesday. Oct 1st, 2014. Beta 2.02<br>
+Thursday. Oct 2nd, 2014. Beta 2.03<br>
+Friday. Oct 3rd, 2014. Beta 2.04<br>
+Thursday. Oct 9th, 2014. Beta 2.05<br>
+Tuesday. Oct 14th, 2014. Beta 2.06<br>
+Wednesday. Oct 29th, 2014. Beta 3.00<br>
+Thursday. Oct 30th, 2014. Beta 3.01<br>
+Thursday. Oct 30th, 2014. Beta 3.02<br>
+Friday. Oct 31st, 2014. Beta 3.03<br>
+Thursday. Nov 6th, 2014. Beta 3.04<br>
+Tuesday. Nov 11th, 2014. Beta 3.05<br>
+Friday. Nov 21st, 2014. Beta 3.9<br>
+Friday. Nov 21st, 2014. Beta 3.9.1<br>
+Saturday. Nov 22nd, 2014. Gamma 1.0<br>
+Monday. Nov 24th, 2014. Gamma 1.01<br>
+Wednesday. Nov 26th, 2014. Gamma 1.02<br>
+Thursday. Nov 27th, 2014. Gamma 1.03<br>
+Friday. Nov 28th, 2014. Gamma 1.04<br>
+Tuesday. Dec 2nd, 2014. Gamma 1.05<br>
+Wednesday. Dec 10th, 2014. Gamma 2.0<br>
+Thursday. Dec 11th, 2014. Gamma 2.01<br>
+Thursday. Dec 11th, 2014. Gamma 2.02<br>
+Friday. Dec 12th, 2014. Gamma 2.03<br>
+Saturday. Dec 13th, 2014. Gamma 2.04<br>
+Sunday. Dec 14th, 2014. Gamma 2.05<br>
+Sunday. Dec 14th, 2014. Gamma 2.05 hotfix build<br>
+Monday. Dec 15th, 2014. Gamma 2.06<br>
+Monday. Dec 15th, 2014. Gamma 2.07<br>
+<b>Tuesday. Dec 16th, 2014. Elite Dangerous v1.0.0.0 !</b><br>
+Wednesday. Dec 17th, 2014. v1.01<br>
+Thursday. Dec 18th, 2014. v1.02<br>
+Friday. Dec 19th, 2014. v1.03<br>
+Monday. Dec 22nd, 2014. Server Update (trading market fixes)<br>
+Wednesday. Dec 24th, 2014. Server Maintenance (no fixes changelog were posted)<br>
+Thursday. Jan 8th, 2015. v1.04<br>
+Thursday. Jan 15th, 2015. v1.05<br>
+Thursday. Jan 22nd, 2015. v1.06<br>
+Wednesday. Jan 28th, 2015. v1.07<br>
+Tuesday. Feb 3rd, 2015. v1.1 Beta &lt;- for alpha/beta backers only<br>
+Thursday. Feb 5th, 2015. v1.1 Beta 2<br>
+Friday. Feb 6th, 2015. v1.1 Beta 3<br>
+Tuesday. Feb 10th, 2015. v1.1 Release<br>
+Tuesday. Feb 10th, 2015. v1.1.01<br>
+Wednesday. Feb 11th, 2015. v1.1.02<br>
+Thursday. Feb 12th, 2015. v1.1.03<br>
+Friday. Feb 13th, 2015. v1.1.04<br>
+Thursday. Feb 19th, 2015. v1.1.05<br>
+Tuesday. Mar 3rd, 2015. v1.2 beta 1<br>
+Wednesday. Mar 4th, 2015. v1.2 beta 2<br>
+Thursday. Mar 5th, 2015. v1.1.06<br>
+Friday. Mar 6th, 2015. v1.2 beta 3<br>
+Monday. Mar 9th, 2015. v1.2 beta 4<br>
+Tuesday. Mar 10th, 2015. v1.2<br>
+Wednesday. Mar 11th, 2015. v1.2.01<br>
+Wednesday. Mar 11th, 2015. v1.2.02 hotfix<br>
+Thursday. Mar 12th, 2015. v1.2.03<br>
+Friday. Mar 13th, 2015. v1.2.04<br>
+Wednesday. Mar 18th, 2015. v1.2.05<br>
+Wednesday. Mar 25th, 2015. v1.2.06<br>
+Friday. Apr 10th, 2015. v1.2.07<br>
+Tuesday. May 26th, 2015. v1.3 beta<br>
+?. May ?th, 2015. v1.3 beta 2<br>
+Thursday. May 28th, 2015. v1.3 beta 3<br>
+Friday. May 29th, 2015. v1.3 beta 4<br>
+Monday. Jun 1st, 2015. v1.3 beta 5 ?<br>
+Tuesday. Jun 2nd, 2015. v1.3 beta 6<br>
+Wednesday. Jun 3rd, 2015. v1.3 beta 7<br>
+Thursday. Jun 4th, 2015. v1.3 beta 8<br>
+Friday. Jun 5th, 2015. v1.3<br>
+Monday. Jun 8th, 2015. v1.3 Server side update<br>
+Tuesday. Jun 9th, 2015. v1.3.01<br>
+Wednesday. Jun 10th, 2015. v1.3.02<br>
+Thursday. Jun 11th, 2015. v1.3.02 Server side update<br>
+Friday. Jun 12th, 2015. v1.3.03<br>
+Tuesday. Jun 16th, 2015. v1.3.04<br>
+Thursday. Jun 18th, 2015. v1.3.05<br>
+Friday. Jun 19th, 2015. v1.3.05 Server side update<br>
+Thursday. Jun 25th, 2015. v1.3.06<br>
+Friday. Jun 26th, 2015. v1.3.06 Server side update<br>
+Thursday. Jul 7th, 2015. v1.3.07<br>
+Wednesday. Aug 5th, 2015. v1.3.08<br>
+Monday. Sep 1st, 2015. v1.4 beta<br>
 </p>
 <h2>Detailed major release times (UTC + 2hrs or 3hrs daylight lameness time):</h2>
 <p>
-v1.3.08<br>
-1336hrs Release
+Beta 3<br>
+2100hrs In-game message for server shutdown in 30 minutes.<br>
+2105hrs Forum post about release, changelog.<br>
+Oct 29th, 2014.<br>
+0009hrs Release.
 <br><br>
-v1.3.06<br>
-1201hrs Servers down<br>
-1230hrs Release
+Nov 21st, 2014.<br>
+0019hrs Forum post about release, changelog.<br>
+0059hrs Release.
 <br><br>
-v1.3.05<br>
-1300hrs Servers down<br>
-1335hrs Release
+Beta 3.9<br>
+2249hrs and 2251hrs game servers went down.<br>
+2259hrs In-game message for server shutdown in 10 minutes.<br>
+2329hrs game servers went down (for real).<br>
 <br><br>
-v1.3.04<br>
-1325hrs Release
+Gamma 1.0<br>
+1409hrs In-game message for server shutdown in 10 minutes.<br>
+1421hrs Forum post about release, changelog.<br>
+1535hrs Release.
 <br><br>
-v1.3.03<br>
-1730hrs Release
+Gamma 2.0<br>
+1832hrs server go down in 60mins in-game message.<br>
+1935hrs game servers down.<br>
+2021hrs release
 <br><br>
-v1.3.02<br>
-2047hrs Release
+Gamma 2.01<br>
+1847hrs server go down in 30mins in-game message.<br>
+1911hrs game servers down.<br>
+1922hrs release
 <br><br>
-v1.3.01<br>
-1903hrs Release
+Gamma 2.02<br>
+2142hrs server go down in 20mins in-game message.<br>
+2208hrs release
 <br><br>
-v1.3<br>
-1658hrs Forum announcement<br>
-1736hrs Servers down<br>
-2116hrs Release
+Gamma 2.03<br>
+1806hrs server go down in 25mins in-game message.<br>
+1845hrs release
 <br><br>
-v1.2.07<br>
-1334hrs Servers down<br>
-1353hrs Release
+Gamma 2.04<br>
+1600hrs release
 <br><br>
-v1.2.06<br>
-1630hrs Server broadcast 30mins servers go down<br>
-1700hrs Servers down<br>
-1713hrs Release
+Gamma 2.05<br>
+1839hrs server go down in 20mins in-game message.<br>
+1942hrs release
 <br><br>
-v1.2.05<br>
-1600hrs Servers down<br>
-1623hrs Release
-<br><br>
-v1.2.04<br>
-?hrs Release
-<br><br>
-v1.2.03<br>
-1730hrs Server broadcast 30mins servers go down<br>
-1801hrs Servers down<br>
-1856hrs Release
-<br><br>
-v1.2.02 hotfix<br>
-1932hrs Servers down<br>
-1935hrs Release
-<br><br>
-v1.2.01<br>
-1705hrs Server broadcast 25mins servers go down<br>
-1730hrs Servers down<br>
-1735hrs Release
-<br><br>
-v1.2<br>
-1635hrs Server broadcast 25mins servers go down<br>
-1713hrs Servers down<br>
-1942hrs Release
-<br><br>
-v1.2 beta 4<br>
-1701hrs Servers down<br>
-1705hrs Release
-<br><br>
-v1.2 beta 3<br>
-1600hrs Servers down<br>
-1603hrs Release
-<br><br>
-v1.1.06<br>
-1755hrs Release
-<br><br>
-v1.2 beta 2<br>
-1830hrs Release
-<br><br>
-v1.2 beta 1<br>
-2038hrs Release
-<br><br>
-v1.1.05<br>
-1813hrs release
-<br><br>
-v1.1.04<br>
-1732hrs servers down<br>
-1736hrs release
-<br><br>
-v1.1.03<br>
-1740hrs server go down in 20mins in-game message.<br>
-?hrs release
-<br><br>
-v1.1.02<br>
-1740hrs server go down in 20mins in-game message.<br>
-1801hrs servers down<br>
-1809hrs release
-<br><br>
-v1.1.01<br>
-2127hrs release
-<br><br>
-v1.1 Release<br>
-1621hrs server go down in 40mins in-game message.<br>
-1700hrs servers down<br>
-1737hrs release
-<br><br>
-v1.1 Beta 3<br>
-1739hrs release
-<br><br>
-v1.1 Beta 2<br>
-1405hrs release
-<br><br>
-v1.1 Beta<br>
-2033hrs release
-<br><br>
-v1.07<br>
-1230hrs server go down in 30mins in-game message.<br>
-1300hrs game servers down.<br>
-1312hrs release
-<br><br>
-v1.06<br>
-?hrs release
-<br><br>
-v1.05<br>
-1235hrs server go down in 25mins in-game message.<br>
-1301hrs game servers down.<br>
-1305hrs release
-<br><br>
-v1.04<br>
-1630hrs game servers down.<br>
-1634hrs release
-<br><br>
-Monday. Dec 22nd, 2014. Server Update (trading market fixes)<br>
-1657hrs server update.
-<br><br>
-v1.03<br>
-1410hrs server go down in 20mins in-game message.<br>
-1431hrs game servers down.<br>
-1440hrs release
-<br><br>
-v1.02<br>
-1405hrs server go down in 30mins in-game message.<br>
-1431hrs game servers down.<br>
-1439hrs release
-<br><br>
-v1.01<br>
-1343hrs server go down in 15mins in-game message.<br>
-1405hrs game servers down.<br>
-1420hrs release
-<br><br>
-Elite Dangerous v1.0.0.0<br>
-1249hrs server go down in 10mins in-game message.<br>
-1304hrs game servers down.<br>
-1400hrs release
+Gamma 2.06<br>
+1713hrs server go down in 15mins in-game message.<br>
+1734hrs game servers down.<br>
+1741hrs release
 <br><br>
 Gamma 2.06<br>
 2353hrs server go down in 25mins in-game message.<br>
@@ -290,54 +187,157 @@ Tuesday, Dec 16th.<br>
 0010hrs game servers down.<br>
 0051hrs release
 <br><br>
-Gamma 2.06<br>
-1713hrs server go down in 15mins in-game message.<br>
-1734hrs game servers down.<br>
-1741hrs release
+Elite Dangerous v1.0.0.0<br>
+1249hrs server go down in 10mins in-game message.<br>
+1304hrs game servers down.<br>
+1400hrs release
 <br><br>
-Gamma 2.05<br>
-1839hrs server go down in 20mins in-game message.<br>
-1942hrs release
+v1.01<br>
+1343hrs server go down in 15mins in-game message.<br>
+1405hrs game servers down.<br>
+1420hrs release
 <br><br>
-Gamma 2.04<br>
-1600hrs release
+v1.02<br>
+1405hrs server go down in 30mins in-game message.<br>
+1431hrs game servers down.<br>
+1439hrs release
 <br><br>
-Gamma 2.03<br>
-1806hrs server go down in 25mins in-game message.<br>
-1845hrs release
+v1.03<br>
+1410hrs server go down in 20mins in-game message.<br>
+1431hrs game servers down.<br>
+1440hrs release
 <br><br>
-Gamma 2.02<br>
-2142hrs server go down in 20mins in-game message.<br>
-2208hrs release
+Monday. Dec 22nd, 2014. Server Update (trading market fixes)<br>
+1657hrs server update.
 <br><br>
-Gamma 2.01<br>
-1847hrs server go down in 30mins in-game message.<br>
-1911hrs game servers down.<br>
-1922hrs release
+v1.04<br>
+1630hrs game servers down.<br>
+1634hrs release
 <br><br>
-Gamma 2.0<br>
-1832hrs server go down in 60mins in-game message.<br>
-1935hrs game servers down.<br>
-2021hrs release
+v1.05<br>
+1235hrs server go down in 25mins in-game message.<br>
+1301hrs game servers down.<br>
+1305hrs release
 <br><br>
-Gamma 1.0<br>
-1409hrs In-game message for server shutdown in 10 minutes.<br>
-1421hrs Forum post about release, changelog.<br>
-1535hrs Release.
+v1.06<br>
+?hrs release
 <br><br>
-Beta 3.9<br>
-2249hrs and 2251hrs game servers went down.<br>
-2259hrs In-game message for server shutdown in 10 minutes.<br>
-2329hrs game servers went down (for real).<br>
-Nov 21st, 2014.<br>
-0019hrs Forum post about release, changelog.<br>
-0059hrs Release.
+v1.07<br>
+1230hrs server go down in 30mins in-game message.<br>
+1300hrs game servers down.<br>
+1312hrs release
 <br><br>
-Beta 3<br>
-2100hrs In-game message for server shutdown in 30 minutes.<br>
-2105hrs Forum post about release, changelog.<br>
-Oct 29th, 2014.<br>
-0009hrs Release.
+v1.1 Beta<br>
+2033hrs release
+<br><br>
+v1.1 Beta 2<br>
+1405hrs release
+<br><br>
+v1.1 Beta 3<br>
+1739hrs release
+<br><br>
+v1.1 Release<br>
+1621hrs server go down in 40mins in-game message.<br>
+1700hrs servers down<br>
+1737hrs release
+<br><br>
+v1.1.01<br>
+2127hrs release
+<br><br>
+v1.1.02<br>
+1740hrs server go down in 20mins in-game message.<br>
+1801hrs servers down<br>
+1809hrs release
+<br><br>
+v1.1.03<br>
+1740hrs server go down in 20mins in-game message.<br>
+?hrs release
+<br><br>
+v1.1.04<br>
+1732hrs servers down<br>
+1736hrs release
+<br><br>
+v1.1.05<br>
+1813hrs release
+<br><br>
+v1.2 beta 1<br>
+2038hrs Release
+<br><br>
+v1.2 beta 2<br>
+1830hrs Release
+<br><br>
+v1.1.06<br>
+1755hrs Release
+<br><br>
+v1.2 beta 3<br>
+1600hrs Servers down<br>
+1603hrs Release
+<br><br>
+v1.2 beta 4<br>
+1701hrs Servers down<br>
+1705hrs Release
+<br><br>
+v1.2<br>
+1635hrs Server broadcast 25mins servers go down<br>
+1713hrs Servers down<br>
+1942hrs Release
+<br><br>
+v1.2.01<br>
+1705hrs Server broadcast 25mins servers go down<br>
+1730hrs Servers down<br>
+1735hrs Release
+<br><br>
+v1.2.02 hotfix<br>
+1932hrs Servers down<br>
+1935hrs Release
+<br><br>
+v1.2.03<br>
+1730hrs Server broadcast 30mins servers go down<br>
+1801hrs Servers down<br>
+1856hrs Release
+<br><br>
+v1.2.04<br>
+?hrs Release
+<br><br>
+v1.2.05<br>
+1600hrs Servers down<br>
+1623hrs Release
+<br><br>
+v1.2.06<br>
+1630hrs Server broadcast 30mins servers go down<br>
+1700hrs Servers down<br>
+1713hrs Release
+<br><br>
+v1.2.07<br>
+1334hrs Servers down<br>
+1353hrs Release
+<br><br>
+v1.3<br>
+1658hrs Forum announcement<br>
+1736hrs Servers down<br>
+2116hrs Release
+<br><br>
+v1.3.01<br>
+1903hrs Release
+<br><br>
+v1.3.02<br>
+2047hrs Release
+<br><br>
+v1.3.03<br>
+1730hrs Release
+<br><br>
+v1.3.04<br>
+1325hrs Release
+<br><br>
+v1.3.05<br>
+1300hrs Servers down<br>
+1335hrs Release
+<br><br>
+v1.3.06<br>
+1201hrs Servers down<br>
+1230hrs Release
+v1.3.08<br>
+1336hrs Release
 </p>
 </section>
 
