87999b83e820bd56acfa1e1b4f7a1c5798a8bb25
Snake Man <snakeman@phnet.fi>
2015-11-23 23:44:58 +0200

    Conflict zones updated.

--- conflict-zones.php
+++ conflict-zones.php
@@ -22,7 +22,7 @@
 <section>
 <h2>Conflict Zones</h2>
 
-<p>These are the locations where factions are at war. Here you can find capital ships and intense combat with many ships in the fight. Combat Bonds (bounties sort of) are not that high as in <a href="resource-extraction-sites.php">Resource Extraction Sites</a> but conflict zones have little bit more action for sure.</p>
+<p>These are the locations where factions are at war. Here you can find capital ships and intense combat with many ships in the fight (but this only appears on big war conflicts, not on smaller civil war conflicts). Combat Bonds (bounties sort of) are not that high as in <a href="resource-extraction-sites.php">Resource Extraction Sites</a> but conflict zones have little bit more action for sure.</p>
 
 <p>Combat income does not affect your rank. It is based on how tough enemies you kill, e.g. if youre novice and destroy a dangerous enemy, you'll get a big rank increase, but no increase at all the other way around.
 <br><br>
@@ -36,6 +36,21 @@ Also, bounties and combat bonds are given a slight passive boost in systems cont
 </p>
 
 <p>Conflict Zone are visually boring as there is no objects other than background galaxy, however they are very hectic in action, they are pure shoot'em ups, a real quake frag fests. There are plenty, sometimes too much, enemy ships flying around as well as friendly as you have to choose a side when you enter to CZ. Often times you run into trouble as you get like three or four enemy ships attacking you at once, then you have mere seconds to live if you're not boosting to escape.
+</p>
+
+<p>Ships are much better not only in combat rank, but in armor as well then their counter parts in RES. For example taking down python in RES and CZ are totally different things.
+</p>
+
+<p>There are constant stream of ships in mad mayhem, RES on the other hand is much more slower pace but that also can have its action moments.
+</p>
+
+<p>You can choose your fights in RES but often times cannot in Conflict Zone. Its very annoyning when your trying to progress your compat cank as Dangerous and that pesky competent cobra attacks you. You cant really ignore him as he does damage while youre chasing higher ranked targets.
+</p>
+
+<p>Even though there is more ships and hectic action, the ship strengths there are much higher so each kill takes longer. When you were peppering that nasty python for like forever, you could have killed several equally ranked ships in RES WHEN LUCKY, of course RES has those nasty downtimes which balances it quite a bit.
+</p>
+
+<p>The bad thing about conflict zones are that if you have very bad luck and get those 4+ ships attacking you at once, its basically game over. Even on anaconda with class 7 A rated shield generator with 5 shield boosters, your shields are toast quicker than your shield cell bank (one) can work to restore them and once your shields are down, the hull goes to 50% or less during the time you are engaging FSD to jump into nearby star system. Very very annoying and you never see this on hazardous RES (unless you provoke the situation yourself by attacking 3+ wings at once).
 </p>
 
 </section>
