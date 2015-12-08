41472c4c983a2336031b92e3afa5d01ec5485210
Snake Man <snakeman@phnet.fi>
2015-12-08 02:08:13 +0200

    Special planets updated.

--- exploration-special-planets.php
+++ exploration-special-planets.php
@@ -62,12 +62,39 @@ S171 19 A 6 b a
 
 <h2>Non-Landable Special Planets</h2>
 <p><b>Ammonia worlds:</b><br>
+Largest:<br>
+7,100km, Snake Sector MS-T C3-10 4<br>
+11,010km, Snake Sector CL-Y D67 A 3<br>
+15,233km, Snake Sector FW-W D1-88 11<br>
+15,915km, Snake Sector Fw-w D1-36 A 5, with rings
 </p>
 
 <p><b>Earth like worlds:</b><br>
+Largest:<br>
+6,185km, Crookaae Zp-m D8-22 C 5<br>
+6,682km, Snake Sector Sj-q B5-4 6<br>
+6,929km, Plaa Aescs Di-i D10-30 3<br>
+7,209km, Byaa Aerb Ze-r E4-12 Ab<br>
+7,337km, Blu Aescs Oy-y D1-42 6<br>
+7,686km, Froals Ru-m D8-31 Ab 3
 </p>
 
 <p><b>Water worlds:</b><br>
+Largest:<br>
+10,336km, Puekoa Sk-c D14-61 A 2<br>
+10,371km, Blo Airm Af-r E4-616 1<br>
+10,612km, Blo Airm Xd-k D8-126 Abc<br>
+10,643km, Plaa Aescs Di-i D10-30 4<br>
+12,102km, Byaa Aerb Lg-n C20-16 3<br>
+12,254km, Snake Sector Kr-w C1-17 2, with rings<br>
+12,383km, Snake Sector Fh-l B8-1 A<br>
+12,406km, Snake Sector Uj-q B5-6 Ab 2<br>
+12,676km, Snake Sector MS-T C3-2 B 1<br>
+12,896km, Snake Sector Ir-w D1-101 2<br>
+15,709km, Snake Sector El-y D108 5, with rings<br>
+16,022km, Snake Sector Fw-w D1-96 Ab 8, with rings<br>
+17,091km, Snake Sector GW-W C1-1 AB 2, with metallic rings<br>
+17,944km, Smojue Kd-a D14-12 1
 </p>
 
 </section>
