75d5c627a14771d796fb730f74c924d72b0e9f06
Snake Man <snakeman@phnet.fi>
2015-12-16 10:00:11 +0200

    Planetary landings guide updated.

--- planetary-landings-guide.php
+++ planetary-landings-guide.php
@@ -3,7 +3,7 @@
 <head>
 <title>PMC Elite Dangerous - Planetary Landings Guide</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous Planetary Landings Guide">
+<META name="description" content="PMC Elite Dangerous Planetary Landings Guide">
 <META name="keywords" content="PMC, Elite Dangerous, Planetary, Landings, Guide">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,7 +20,7 @@
 </header>
 
 <section>
-<p>Basic info you need to know to set down on those planets. Planetary Landings is called Elite Horizons, otherwise known as Elite Dangerous v2.0 and also known as Season 2.</p>
+<p>Basic info you need to know to set down on those planets. Planetary Landings is called Elite Dangerous: Horizons, otherwise known as Elite Dangerous v2.0 and also known as Season 2.</p>
 
 <p>If you have purchased only Elite Dangerous v1.0 (upgraded to v1.5 in december 2015), then you cannot land on planets.
 </p>
@@ -28,12 +28,12 @@
 <p>The glide currently disengages at ~7km above (an approximate) floor height, 11km short of face-firsting into the landscape, or when you come in too steep (60 degrees plus).
 </p>
 
-<h2>Planetary Ship Scanner</h2>
-<p>In the ship a blue circle on your scanner will indicate a nearby point of interest.
+<h2>Planetary Ship Scanner / Sensors</h2>
+<p>In the ship a blue circle on your scanner will indicate a nearby point of interest or anomaly.
 </p>
 
 <h2>Ship Heads Up Display (HUD) Indicators</h2>
-<p>Ship HUD indicators: OC is orbital cruise, DRP is drop (out of orbital cruise). Terrain scanner appears below 75m altitude.
+<p>Ship HUD indicators: OC is orbital cruise, DRP is drop (out of orbital cruise). Terrain scanner appears below 75m altitude or when your landing gear are deployed.
 </p>
 
 <h2>SRV Wave Scanner</h2>
@@ -65,8 +65,131 @@
 <p>Be very very careful while exploring and landing on planets.
 </p>
 
-<h2>Planetary Ports / Outposts</h2>
-<p>When you are in a ship landing into planetary settlement / outpost, be very very careful with the approach as there is gravity involved. It is not the same as docking your ship in space, you sink like crazy in the planets depending on the planets gravity.
+<h2>Planetary Ports</h2>
+<p>When you are in a ship landing into planetary port, be very very careful with the approach as there is gravity involved. It is not the same as docking your ship in space, you sink like crazy in the planets depending on the planets gravity.
+</p>
+
+<h2>Planetary Landings Guide - Landing</h2>
+<p>Your ship needs Planetary Approach Suite for surface landings otherwise it will stop above orbital cruise zone of a planet.
+</p>
+
+<p>You can see which planets are landable by the navigation panel blue ship icon, or from system map by the blue circle around the planet.
+</p>
+
+<p>For entering orbital cruise, you must be lower than 200km/s speed. Orbital cruise is the area around planet where you can travel very quickly.
+</p>
+
+<p>In HUD the OC indicates orbital cruise, DRP indicates where you drop out of orbital cruise and enter glide mode.
+</p>
+
+<p>Glide mode is for your ship to travel great distance between orbital cruise and planetary flight.
+</p>
+
+<p>Planetary flight is very similar to normal flight while in space.
+
+PMCTODO https://www.youtube.com/watch?v=O2uYM2ykS1I
+</p>
+
+<p>The orbital HUD: the order is now; latitude, longitude, gravity
+</p>
+
+<h2>Planetary Landings Guide - SRV</h2>
+<p>Before you can deploy and drive SRV on planets surface, you first need to purchase two items from outfitting; Planetary Vehicle Hangar and Vehicle Bay.
+</p>
+
+<p>Remember, you must have Planetary Approach Suite fitted to your ship as well.
+</p>
+
+<p>The first time you buy vehicle bay, it comes with SRV equipped as standard.
+</p>
+
+<p>If your are destroyed vehile manning a SRV, you will be returned to your ship at the orbit. At this point you must restock your vehicle bay with another SRV. These can be purchased at restock option at startport services.
+</p>
+
+<p>At planets surface you deploy SRV by looking down with trackir or using UI focus button and press down to open the role switch panel.
+</p>
+
+<p>When you are in the SRV, your ship will stay behind unless you dismiss it or travel further than 2km away from it. When this happens your ship engages auto pilot and flies to planets orbit where it remains safe from any danger.
+</p>
+
+<p>The SRV consumes fuel and the way you drive, use boost and which modules are powered all affect the rate of consumption. Driving at lower speeds help to decrease fuel use and has the useful side effect that makes it easier to traverse rocky terrain. Powering down modules also reduces fuel consumption.
+</p>
+
+<p>If you do run out of fuel, your life support and emergency fuel will kick in giving you around 4 minutes to do one of two things; either get back to your ship and refuel at port which is automatic, or find materials and synthesise fuel. If you fail to to either, your SRV eventually runs out of fuel completely and self destruct, you'll be then transported to your ship. If this happens, follow the restock process if you wish to continue to explore in SRV.
+</p>
+
+<p>To get back into your ship, you must drive to its location or recall it. You can use the recall ship option in the role switch panel (look down or UI focus + down button). You cannot recall your ship when in close proximity to a settlement, the recall ship option in role switch panel will indicate how far you must drive away from the settlement in order to recall your ship in.
+</p>
+
+<p>Once back at your ship, you see small hologram under the ship showing the location of vehicle hangar, drive towards this. Once in the correct position, the board ship indicator located below the fuel panel will light up blue. Select the option board ship from the role switch panel to complete the retrieval process.
+</p>
+
+<p>When you are in settlement / port, you can get back to your docked ship by requesting docking. Use the compass to locate the hangar your ship is docked, drive in until the board ship indicator turns blue. You can now board your ship.
+</p>
+
+<p><b>Snakes tips:</b>
+</p>
+
+<p>When exploring in SRV there are some things to consider.
+</p>
+
+<p>When you see incredible deep canyon, think twice before actually driving / dropping there as it might be not possible to get out if your ship wont land there when recalled.
+</p>
+
+<p>You might end up traveling dozens of kilometers through that canyon bottom before you find a way to climb out or wide enough area for your ships AI to decide to land on. This is especially true for ice worlds where its very slippery and slope which you could climb in rocky worlds, is not possible in the icy environment.
+</p>
+
+<h2>Planetary Landings Guide - Wave Scanner</h2>
+<p>In your ship flying above 2km altitude, the ships sensors will detect anomalies indicated by blue circular area. You can increase the sensor range so you can detect anomalies from further away. Below 2km altitude sensors cannot detect them due ground intereference.
+</p>
+
+<p>The area on the sensor disc does not pinpoint the location of any objects that may be found in it. Its best to use the SRV to locate them.
+</p>
+
+<p>Land and use your SRV with wave scanner to find them.
+</p>
+
+<p>SRV Wave Scanner<br>
+<img src="images/srv-wave-scanner.png" alt="SRV Wave Scanner">
+</p>
+
+<p>Wave scanner is forward arc passive scanner that can detect signatures from variety of objects.
+</p>
+
+<p>At distance the objects signature will take up large left to right section. As you head towards the signature and decrease the distance, the elements get brighter and the signal is getting narrower in the scanner. Note; the scanner does not indicate distance, patterns on the top are not further away than those on the bottom.
+</p>
+
+<p>Each object has its own unique signal that will be displayed on the scanner, as well as audio cue.
+</p>
+
+<p>Once you have located material outcrop, target it and shoot it with your turret. This will break it down to smaller fragments which your SRV can cargo scoop. Lower the cargo scoop, target a fragment and drive over it to scoop it onboard.
+</p>
+
+<p>You can see the amounts you have collected in the materials section in the inventory tab in right hand panel.
+</p>
+
+<p>Your CMDR has 300 pieces worth of space for materials, these materials are not connected to your ships cargo or SRV, they are connected to your CMDR. If your SRV/ship is destroyed the materials are NOT lost.
+</p>
+
+<h2>Planetary Landings Guide - Settlements and Surface Ports</h2>
+<p>Unlike starports or outposts in space, ground based installations only show up in navigation panel if you have discovered them or bought exploration data for the planet they are on.
+</p>
+
+<p>There are two types of settlements / surface ports in planet surfaces; surface port is where you can dock and settlement is where you cannot dock but can interact with the SRV.
+</p>
+
+<p>Ship can dock icon<br>
+<img src="images/planetary-surface-port-icon.png" alt="Planetary Surface Port Icon">
+</p>
+
+<p>Only SRV can interact icon<br>
+<img src="images/planetary-settlement-icon.png" alt="Planetary Settlement Icon">
+</p>
+
+<p>Settlements come in sizes from tiny to large with different security levels. Settlement size is denoted by plus (+) sign next to their name ranging from none to maximum of three. Security level is shown as part of the name in [brackets].
+</p>
+
+<p>Low security means little threat to CMDR entering in SRV, medium will include some no-tresspass zones and high means they are dangerous places to visit. All settlements have no fly zones protecting them. You can enter these areas (with your ship) but its illegal to do so, if you do not leave the zone withing given time, you'll be fined and the settlement will become hostile.
 </p>
 
 </section>
