b96a581c87356746704a6ef27c5ba82d25667bc2
Snake Man <snakeman@phnet.fi>
2015-12-16 20:50:22 +0200

    Author name added.

--- edmc.php
+++ edmc.php
@@ -17,12 +17,12 @@
 <?php include("../include/google-ads.php"); ?>
 <?php include("../include/paypal.php"); ?>
 <h1><b>PMC Elite Dangerous, Market Connector (EDMC)</b></h1>
-<p>By ?</p>
+<p>By Otis B.</p>
 </header>
 
 <section>
 <h2>Elite Dangerous Market Connector</h2>
-<p>Uses Frontier Developments official iOS API to connect and download commodity market prices, shipyard ship sales and outfitting modules information. It then sends this data automatically to <a href="eddn.php">EDDN</a> with single click of a button.
+<p>Uses Frontier Developments official iOS API to connect and download commodity market prices, shipyard ship sales and outfitting modules information. It then sends this data automatically to <a href="eddn.php">EDDN</a> with single click of a button, or a keyboard hotkey.
 </p>
 
 <p>Early days of Elite we had to use manual pencil and paper, then we got the OCR solutions which were somewhat cumbersome and poor, but now finally we have EDMC which uses the API, makes no mistakes and is automatic (you only have to click a button).
