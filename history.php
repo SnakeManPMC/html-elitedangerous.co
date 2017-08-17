bc543e8594570b7e8e7b8e1c190cff88ed4a8315
Snake Man <snakeman@phnet.fi>
2017-08-17 16:36:59 +0300

    v2.4 beta 1 history added.

--- history.php
+++ history.php
@@ -191,7 +191,8 @@ Wednesday. May 17th, 2017. v2.3.02 Server side update<br>
 Tuesday. Jun 6th, 2017. v2.3.10 (beta 1?)<br>
 Monday. Jun 12th, 2017. v2.3.10 Beta 2<br>
 Monday. Jun 26th, 2017. v2.3.10<br>
-Tuesday. Jul 11th, 2017. v2.3.11
+Tuesday. Jul 11th, 2017. v2.3.11<br>
+Thursday. Aug 17th, 2017. v2.4 beta 1
 </p>
 <h2>Detailed major release times (UTC + 2hrs or 3hrs daylight lameness time):</h2>
 <p>
@@ -640,6 +641,9 @@ v2.3.10<br>
 <br><br>
 v2.3.11<br>
 1132hrs patch notes
+<br><br>
+v2.4 Beta 1<br>
+1309hrs patch notes
 </p>
 </section>
 
