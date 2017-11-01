cc1798d1a70c5e01c9bb6325ded008befa0a194f
Snake Man <snakeman@phnet.fi>
2017-11-01 23:27:45 +0200

    maddavos-market-share.php page added.

---
+++ maddavos-market-share.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PMC Elite Dangerous - Maddavo's Market Share</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="PMC, Elite Dangerous, Maddavo's Market Share">
+<META name="keywords" content="PMC, Elite Dangerous, Maddavo, Market, Share, MMS">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("include/google-ads.php"); ?>
+<h1><b>PMC Elite Dangerous - Maddavo's Market Share</b></h1>
+<p>Maddavo's Market Share (MMS)</p>
+</header>
+
+<section>
+<h2>Maddavo's Market Share</h2>
+<p>Maddavo's Market Share is the main data hub for <a href="tradedangerous.php">Trade Dangerous</a> (TD) trading tool.
+</p>
+
+<p>You can download systems, stations, market prices and ship vendors as CSV file format which TD reads. There is also editing option for station data like distance from star, landing pad size and various others.
+</p>
+
+<p>Maddavo Market Share guide
+</p>
+
+<p>Delete station by adding -1 as its distance from star and upload the .csv (or use editstation.asp page)
+</p>
+
+<p><a href="http://www.davek.com.au/td/">Maddavo's Market Share (MMS)</a> homepage.
+</p>
+
+</section>
+
+<footer>
+<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
+<br><br>
+<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y"));?>.</i></p>
+<?php include("include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
