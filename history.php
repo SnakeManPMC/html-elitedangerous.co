335aaef432098e8aa9d3122648d29b060b23fd97
Snake Man <snakeman@phnet.fi>
2019-09-23 14:47:19 +0300

    v3.5 patch 1 history added

--- history.php
+++ history.php
@@ -230,7 +230,8 @@ Tuesday. Feb 19th, 2019. v3.3.05<br>
 Tuesday. Apr 23rd, 2019. v3.4 "April Update"?<br>
 Tuesday. May 14th, 2019. v3.4 "April Update - Patch 2"???<br>
 Wednesday. Sep 18th, 2019. v3.5 "September Update"<br>
-Thursday. Sep 19th, 2019. v3.5 Server Side Update
+Thursday. Sep 19th, 2019. v3.5 Server Side Update<br>
+Monday. Sep 23rd, 2019. v3.5 Patch 1
 </p>
 <h2>Detailed major release times (UTC + 2hrs or 3hrs daylight lameness time):</h2>
 <p>
@@ -796,6 +797,9 @@ Patch notes arrived already the previous day
 <br><br>
 v3.5 Server Side Update<br>
 1929hrs patch notes
+<br><br>
+v3.5 Patch 1<br>
+1347hrs patch notes
 </p>
 </section>
 
