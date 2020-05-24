26a6c65d0ec16d8f657a174d1e27bb6656dc996b
Snake Man <snakeman@phnet.fi>
2020-05-24 18:25:06 +0300

    Scan data sending info added

--- edmc.php
+++ edmc.php
@@ -30,6 +30,14 @@
 <p>Early days of Elite we had to use manual pencil and paper, then we got the OCR solutions which were somewhat cumbersome and poor, but now finally we have EDMC which uses the API, makes no mistakes and is automatic (you only have to click a button).
 </p>
 
+<p>
+When you returned from a long exploration journey with thousands of system scans and you have used the option to delay sending scan data to EDDN until docked, EDMC will start to send that data once you dock. Having thousands of scans cause EDMC to send that data for a quite while as it sends it slowly not to flood EDDN.
+</p>
+
+<p>
+When sending scan data and you close EDMC no data gets lost as next time when you start EDMC and have elite running, the sending of scan data continues. Note that if you just have EDMC running alone it will not send scan data, you must start elite and be docked for EDMC to send data.
+</p>
+
 <p><a href="https://github.com/Marginal/EDMarketConnector">EDMC Homepage</a> in github
 </p>
 
