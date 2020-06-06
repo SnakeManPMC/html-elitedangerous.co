0d168421971d62de349a451ffc06d5dfac8c5faf
Snake Man <snakeman@phnet.fi>
2020-06-06 10:13:53 +0300

    Emptied obsolete page

--- pmc-most-wanted.php
+++ pmc-most-wanted.php
@@ -21,16 +21,8 @@
 <section>
 <h2>PMC Most Wanted</h2>
 
-<p><b>Cmdr ECNIRP</b> - 01-24-15, 0000hrs used ASP and collided with my lakon type-9 causing 744,000cr damages. This was in BD-02 4304 system Durrance station. He then jumped into Santjalan system and we lost track of him there.</p>
-
-<p><b>Cmdr LooseKiller</b> - killed me on 03-19-15 with Python against my unarmed/shieldless lakon type-9 WHILE I WAS DOCKING. He killed me again 03-21-15 with Vulture after interdiction, this time I had turreted beams and was running empty thank god.</p>
-
-<p>My losses on insurance 3.6 mil first time, 3.9 mil second time. This happened BD-02 4304, near bean enterprise.
-2 Cmdr ECNIRP, collided with me intentionally using his ASP, did not reply to comms. damages 745,000cr to my lakon type-9.</p>
-
-<p><b>Cmdr John Charger</b> - 2015-04-26T23:24:05Z, Synteini: I was with wing in Majkl who was interdicted in his Lakon Type-9, player killer was in python. I dropped into the same interdiction as I was following Majkl's lead. Player killer destroyed me first very very quickly. Afterwards I heard Majkl managed to get into super cruise, but was interdicted again so he combat logged after seeing what happened to me.</p>
-
-<p><b>Update:</b> on late 2015 it was clear that open is for player killers only and normal player versus environment CMDR's use private groups like Mobius. Since long time I have not flown in open just for this reason. This page is kind of obsolete as nobody is going to claim this "most wanted" bounty I guess you could call it, but decided to leave this page as historical reminder of the Elite's past.
+<p>
+<b>2020-06-06</b> This page was deleted as obsolete, sorry about that.
 </p>
 
 </section>
