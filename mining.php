e7686206ad4175e1205f8f626fc1d6b318bab7d5
Snake Man <snakeman@phnet.fi>
2020-07-04 11:20:19 +0300

    Latest mining knowledge added

--- mining.php
+++ mining.php
@@ -19,33 +19,49 @@
 </header>
 
 <section>
+<p>
+<b>2020-07-04</b> last update.
+</p>
+
 	<h2>Mining</h2>
 
 <p>
 Mining is shooting a laser to an asteroid which gives fragments, collector limpets grab these fragments and bring them to your ship where refinery will process them into minerals which are placed in your cargo hold. You can sell these minerals in many stations.
 </p>
 
+<p>
+<b>Types of mining?</b>
+</p>
+
+<p>
+Basic three types of mining are: laser mining, sub surface deposit mining and deep core mining. Laser mining is definitely the most common and easiest way of mining which only requires a mining laser to get fragmetns out of an asteroid. Sub surface deposit mining requires drill missiles which extract the deposits to eject mineral fragments. Deep core requires seismic charges and complex "dance" to blow up asteroid in spectacular manner.
+</p>
+
 	<h2>Outfitting Mining Ship</h2>
 
 <p>
-Mining Outfit:
+Laser Mining Outfit:
 </p>
 <ul>
-<li>detailed surface scanner so you can find the hotspots (you need it even after you have scanned them)</li>
+<li>detailed surface scanner so you can find the hotspots (you need it even after you have scanned rings)</li>
 <li>prospector limpet controller</li>
 <li>collector limpet controller, as many as you can have, lakon type-9 can have 11 limpets</li>
 <li>mining lasers</li>
 <li>refinery</li>
 <li>cargo racks, as many as you can have</li>
-<li>fuel scoop</li>
+<li>fuel scoop (optional, see below)</li>
 </ul>
 
 <p>
-You need fuel scoop which in general is optional, but the distances you need to travel between mining / selling systems are so long that you save time with fuel scoop not needing to dock for refuels. For those wealthy commanders with fleet carriers, fuel scoop is excess weight they don't need it as their carrier is nearby.
+You need fuel scoop which in general is optional, but the distances you need to travel between mining / selling systems are so long that you save time with fuel scoop not needing to dock for refuels. For those wealthy commander's (CMDR) with fleet carriers, fuel scoop is excess weight they don't need it as their carrier is nearby.
 </p>
 
 <p>
-Don't forget to purchase limpets from stations / carriers as limpet controllers are useless without limpets.
+<b>Shields</b> beginner commanders should definitely use shields because it is very easy to fly into asteroids or have them clip your ship while you are not even moving (asteroid slowly rotates and "long" part hits your ship). Even experienced Triple Elite commanders often return from a mining trip with hull damaged some ways :)
+</p>
+
+<p>
+Don't forget to purchase limpets from stations / carriers as limpet controllers are useless without limpets. Same goes to sub surface deposit drill missiles.
 </p>
 
 	<h2>Finding A Planet With Rings</h2>
@@ -67,19 +83,21 @@ If you have not detail surface scanned the rings before you need to do so to fin
 </p>
 
 <p>
-After detailed surface scanning check visually the orange/yellow hotspots in the rings and look left side panel navigation tab for lockable locations for these hotspots. When you see overlapping hotspots of same type minerals, painite, low temperature diamonds, tritium etc, then you have found something good.
+After detailed surface scanning check visually the orange/yellow hotspots in the rings and look left side panel navigation tab for lockable locations for these hotspots. When you see overlapping hotspots of same type minerals, painite, LTD, tritium etc, then you have found something good.
 </p>
 
 <p>
-Double overlapping hotspots are very nice and triple overlapping hotspots are like you really struck gold now. At the moment of writing this page (2020-06-19) there are only two known locations discovered by CMDRs for LTD triple overlapping hotspots :)
+Double overlapping hotspots are very nice and triple overlapping hotspots are like you really struck gold now. At the moment of writing this page there are only three known locations discovered by CMDRs for LTD triple overlapping hotspots :)
 </p>
-<pre>
-Col 285 Sector CC-K a38-2 1
-Corona Austr. Dark Region OX-U b2-3 6
-</pre>
 
 <p>
-Asteroid belts are not worth mining, go to planet rings instead.
+<a href="https://www.edsm.net/en/system/id/3165155/name/Col+285+Sector+CC-K+a38-2">Col 285 Sector CC-K a38-2 1</a><br>
+<a href="https://www.edsm.net/en/system/id/7531161/name/Corona+Austr.+Dark+Region+OX-U+b2-3">Corona Austr. Dark Region OX-U b2-3 6</a><br>
+<a href="https://www.edsm.net/en/system/id/53445988/name/Swoilz+PK-R+b20-0">Swoilz PK-R b20-0 1</a>
+</p>
+
+<p>
+Asteroid belts are not worth mining, go to planet rings instead. Not only rings but hotspots of specific mineral you want to mine. Not only hotspots but overlapping hotspots, the more the better.
 </p>
 
 	<h2>AI Ships (pirates)</h2>
@@ -93,7 +111,7 @@ AI ships? Yes because never ever should you be mining/trading in Open Play becau
 </p>
 
 <p>
-When you drop to the ring asteroids system authority vessels or miner or pirates or mixture of these appear, police/pirates will scan you. If a lone miner appears he just does his thing and doesn't bother you. If you get scanned by a pirate and have cargo on board, get ready to high wake jump out of there because the pirate will attack you. You can obey and jettison cargo (do not abandon) and then let your collector limpets pick them right back up heh.
+When you drop to the ring asteroids, system authority vessels or miner or pirates or mixture of these appear, police/pirates will scan you. If a lone miner appears he just does his thing and doesn't bother you. If you get scanned by a pirate and have cargo on board, get ready to high wake jump out of there because the pirate will attack you. You can obey and jettison cargo (do not abandon) and then let your collector limpets pick them right back up heh.
 </p>
 
 <p>
@@ -122,7 +140,7 @@ If you own a carrier or hitch a ride on some other CMDRs carrier then you have n
 	<h2>Shooting Asteroid, the real mining</h2>
 
 <p>
-Choosing an asteroid: shoot prospector limpet to it and target the limpet. Do not start mine an asteroid that has less than 20% precious mineral content. Notice that painite has much higher percentage content than lets say low temperature diamonds.
+Choosing an asteroid: shoot prospector limpet to it and target the limpet. Do not start mine an asteroid that has less than 20% precious mineral content. Notice that painite has much higher percentage content than lets say LTDs.
 </p>
 
 <p>
@@ -134,17 +152,68 @@ Even if your cargo hold is full you can still store stuff in the refinery bins, 
 </p>
 
 <p>
-Void opals pristine or depleted ring doesn't matter (game doesn't even include that info anymore?), what matters is rings MASS, it should be over trillion. That sort of mass increases the chances of void opals appearing. Overlapping hotspots does not matter when it comes to void opals.
+Overlapping hotspots does not matter when it comes to void opals.
 </p>
 
 <p>
-Low Temperature Diamonds overlapping hotspots mining is still the best in a large ship with many collector limpets.
+LTDs overlapping hotspots mining is still the best in a large ship with many collector limpets.
+</p>
+
+	<h2>Fleet Carriers</h2>
+
+<p>
+In the bubble Col 285 "mines" ie Kirre's Icebox, there are probably more than hundred fleet carriers at any given time. These carriers either buy directly from the miner in the (nearby) system, or they will taxi miners to a highest paying / selling station hundreds of light years away.
+</p>
+
+<p>
+HOWTO find carriers? Use system chat. Activate system chat by going to your comms panel (top left of your ship HUD), switch to the right most TAB which is settings "cog wheel" icon, in there setup system as "MAIN", now all the system chat across all the game-modes; open and solo, will be shown in the comms panel and you can chat with everybody when you use TAB key to switch into LOCAL to SYSTEM mode.
+</p>
+
+<p>
+Once you activated system chat you should immediately start to see a rich chatter in the comms panel. Of course this depends on time of day, UTC morning to noon is a bit more quiet, but even then if there is more than 10 minutes of silence, your chat or Elite server connection (port forwarding?) is not working properly. Usually there are no issues seeing the chat but few reports tells us that for no apparent reason very rarely some CMDRs cant see chat, restarting Elite client and/or checking connection issues usually has fixed this.
+</p>
+
+<p>
+When you watch the chat you most likely very soon see carrier CMDRs announcing LTD buying or departures to highest selling system. If there are no such announcements you can just ask "Any carrier leaving to selling system?" or similar.
+</p>
+
+<p>
+Word of warning; often times the system chat gets trolls or just otherwise players who think they are anonymous and can say any offensive thing that comes to their mind. Try not to get involved in such conversations, just ignore them. Be friendly and positivie in the system chat, lets all try to make it better, more enjoyable place for mining CMDRs.
+</p>
+
+<p>
+Once you find a carrier buying minerals or departing to the selling system, just head over to it and dock, you might have to jump to a next star system. Its often times very difficult to find the star system name from chat, then browse it from navigation panel or type into galaxy map search. Searching for carrier NAME in systemp map or navigation panel is horror, there are no alphabetical order and the lists (at least in Col 285 Sector Cc-k a38-2) are very long.
+</p>
+
+<p>
+<b>Payment for a taxi?</b>
+</p>
+
+<p>These taxi rides do not have a technical method of payment, but most CMDRs donate either tritium or LTDs. For carrier CMDRs tritium while it can be used as fuel is far less valuable as LTDs, if you are wondering what to donate then simply donating/selling ONE LTD is just fine, that will cover the costs for fuel it takes to taxi you. Donating assumes carrier CMDR has put really low price on LTD buy order, like carrier is bying LTDs for 2000 credits that clearly means this carrier wants LTD donations as taxi fee.
+</p>
+
+<p>
+<b>Selling directly to carrier?</b>
+</p>
+
+<p>When carrier taxi ferrys you to a selling station it takes 20 minutes for the countdown assuming you arrived before or just when the countdown starts, then you go to sell however long this might take, then you return to a carrier and travel back to the LTD3 rings which takes another 20 minutes. This is overall 40 minutes of time used. If you mine and sell minerals directly to carrier it will take far less time, you dock, sell and jump / super cruise right back to the rings to mine some more. How much can you mine in 40 minutes as that's long a carrier ride would take. Selling directly to carriers is good for mining CMDR and the carrier CMDR as well.
+</p>
+
+<p>
+Carriers buying directly from miners have various price ranges, you should know what the current highest paying station is at (see below) before choosing which carrier to sell to. It is pointless to write numbers into this page as these numbers most likely have changed after 24hrs, but lets just say that a <i>very rough</i> ballbark would be that miner should not sell LTDs with less than 1 million per ton.
+</p>
+
+<p>
+<b>What is the "sell bug"?</b>
+</p>
+
+<p>In v3.7 fleet carriers patch 2 there is a bug that prevents CMDRs to sell into a carrier if the buy order is maxed out against carriers credit balance. What happens is that CMDR tries to sell, chooses sell amount (usually full cargo) and clicks "Sell" button... but nothing happens, the selling dialog just closes. Sometimes this can be avoided by selling a very low amounts, 10, 5 or even 1 ton at once which obviously is terrible experience if you have to sell hundreds of tons. Often times there are carriers which buy orders simply do not work at all, even if you try to sell 1 ton at a time it doesn't work, in these cases just move on to a next carrier. Note: this most likely will be fixed in the next patch, well we can hope, can we.
 </p>
 
 	<h2>Selling Minerals</h2>
 
 <p>
-Search for stations buying expensive mineral by using <a href="https://eddb.io/commodity/83">eddb commodity painite</a>, <a href="https://eddb.io/commodity/276">eddb commodity low temperature diamonds</a> or <a href="tradedangerous.php">TradeDangerous</a>:
+Search for stations buying expensive mineral by using <a href="https://eddb.io/commodity/83">eddb commodity painite</a>, <a href="https://eddb.io/commodity/276">eddb commodity low temperature diamonds</a>, <a href="https://inara.cz/galaxy-commodity/144/">inara commodity low temeprature diamonds</a> or <a href="tradedangerous.php">TradeDangerous</a>:
 </p>
 
 <p>
@@ -180,7 +249,7 @@ If you can find bulletin board missions which buy minerals they pay great deal f
 </p>
 
 <p>
-2020-06-06 Few days out from fleet carriers release and everyone seems to be in low temperature diamonds (LTD) laser mining frenzy on Borann system A 2 ring. People say LTD laser mining is now the most profitable mining.
+2020-06-06 Few days out from fleet carriers release and everyone seems to be in LTD laser mining frenzy on Borann system A 2 ring. People say LTD laser mining is now the most profitable mining.
 </p>
 
 <p>
