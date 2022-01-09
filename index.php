c404cb5a7f1e04169c72acf1d5a526d1171b7e72
Snake Man <snakeman@phnet.fi>
2022-01-09 10:03:44 +0000

    index HTML lang en and paragraps added

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
@@ -20,95 +20,106 @@
 
 <section>
 
-<p>Welcome to PMC Tactical's Elite Dangerous pages. We are the old school PMC Space Monkey's from 1984 :)</p>
-<p>These pages are just <b>CMDR Snake Man's personal notes</b>, written since start of Premium Beta testing, these are not meant to be mainstream web site stuff. If some of the pages are out of context, odd, incomplete or off color whatever, now you know why.</p>
+<p>
+Welcome to PMC Tactical's Elite Dangerous pages. We are the old school PMC Space Monkey's from 1984 <img src="../images/smileys/icon_smile.gif" alt="Smiley :)" loading="lazy">
+</p>
+
+<p>
+These pages are just <b>CMDR Snake Man's personal notes</b>, written since start of Premium Beta testing, these are not meant to be mainstream web site stuff. If some of the pages are out of context, odd, incomplete or off color whatever, now you know why.
+</p>
 
 <div class="icat">
-<h2>Combat</h2>
-<a href="bounty-hunting.php">Bounty Hunting</a><br><br>
-<a href="conflict-zones.php">Conflict Zones</a><br><br>
-<a href="resource-extraction-sites.php">Resource Extraction Sites (RES)</a><br><br>
-<a href="compromised-navigation-beacons.php">Compromised Navigation Beacons</a><br><br>
-<a href="weapons.php">Weapons</a><br><br>
-<a href="combat-analysis.php">Combat Analysis</a> with difference ships
+	<h2>Combat</h2>
+
+<p><a href="bounty-hunting.php">Bounty Hunting</a></p>
+<p><a href="conflict-zones.php">Conflict Zones</a></p>
+<p><a href="resource-extraction-sites.php">Resource Extraction Sites (RES)</a></p>
+<p><a href="compromised-navigation-beacons.php">Compromised Navigation Beacons</a></p>
+<p><a href="weapons.php">Weapons</a></p>
+<p><a href="combat-analysis.php">Combat Analysis</a> with difference ships</p>
 </div>
 
 <div class="icat">
-<h2>Trading</h2>
-<a href="commodities-market.php">Commodities Market</a><br><br>
-<a href="eddb.php">EDDB</a>, <a href="eddn.php">EDDN</a>, <a href="edmc.php">EDMC</a><br><br>
-<a href="eliteocr.php">EliteOCR</a> by seeebek<br><br>
-<a href="tradedangerous.php">TradeDangerous</a> by kfsone<br><br>
-<a href="landing-pad-size-distance-from-star-tutorial.php">Landing pad size and distance from star tutorial</a><br><br>
-<a href="thrudds.php">Thrudds Obsolete elite trading tool</a><br><br>
-<a href="trading.php">Trading Basic Info</a><br><br>
-<a href="improving-trading-data.php">Improving Trading Data</a><br><br>
-<a href="maddavos-market-share.php">Maddavo's Market Share</a>
+	<h2>Trading</h2>
+
+<p><a href="commodities-market.php">Commodities Market</a></p>
+<p><a href="eddb.php">EDDB</a>, <a href="eddn.php">EDDN</a>, <a href="edmc.php">EDMC</a></p>
+<p><a href="eliteocr.php">EliteOCR</a> by seeebek</p>
+<p><a href="tradedangerous.php">TradeDangerous</a> by kfsone</p>
+<p><a href="landing-pad-size-distance-from-star-tutorial.php">Landing pad size and distance from star tutorial</a></p>
+<p><a href="thrudds.php">Thrudds Obsolete elite trading tool</a></p>
+<p><a href="trading.php">Trading Basic Info</a></p>
+<p><a href="improving-trading-data.php">Improving Trading Data</a></p>
+<p><a href="maddavos-market-share.php">Maddavo's Market Share</a></p>
 </div>
 
 <div class="icat">
-<h2>Exploration</h2>
-<a href="exploration.php">Exploration</a><br><br>
-<a href="exploration-special-planets.php">Special Planets</a><br><br>
-<a href="system-map.php">System Map Guide</a><br><br>
-<a href="cockpit-holograms.php">Cockpit Holograms</a><br><br>
-<a href="hyperspace-jump.php">Hyperspace Jump</a><br><br>
-<a href="super-cruise.php">Super Cruise</a><br><br>
-<a href="star-systems.php">Star Systems</a><br><br>
-<a href="planetary-landings-guide.php">Planetary Landings Guide</a><br><br>
-<a href="edsm.php">EDSM</a><br><br>
-<a href="captains-log.php">Captain's Log</a> by Genar-Hofoen<br><br>
-<a href="visited-stars-cache.php">Visited Stars Cache</a>
+	<h2>Exploration</h2>
+
+<p><a href="exploration.php">Exploration</a></p>
+<p><a href="exploration-special-planets.php">Special Planets</a></p>
+<p><a href="system-map.php">System Map Guide</a></p>
+<p><a href="cockpit-holograms.php">Cockpit Holograms</a></p>
+<p><a href="hyperspace-jump.php">Hyperspace Jump</a></p>
+<p><a href="super-cruise.php">Super Cruise</a></p>
+<p><a href="star-systems.php">Star Systems</a></p>
+<p><a href="planetary-landings-guide.php">Planetary Landings Guide</a></p>
+<p><a href="edsm.php">EDSM</a></p>
+<p><a href="captains-log.php">Captain's Log</a> by Genar-Hofoen</p>
+<p><a href="visited-stars-cache.php">Visited Stars Cache</a></p>
 </div>
 
 <div class="icat">
-<h2>Game Releases</h2>
-<a href="beta-3.php">Beta 3 notes</a><br><br>
-<a href="final-release.php">Final Release</a><br><br>
-<a href="gamma.php">Gamma (Release Candidate)</a><br><br>
-<a href="history.php">History</a><br><br>
-<a href="news.php">News or some snippets</a><br><br>
-<a href="price-history.php">Price history</a><br><br>
-<a href="expansion-seasons.php">Expansion Seasons</a>
+	<h2>Game Releases</h2>
+
+<p><a href="beta-3.php">Beta 3 notes</a></p>
+<p><a href="final-release.php">Final Release</a></p>
+<p><a href="gamma.php">Gamma (Release Candidate)</a></p>
+<p><a href="history.php">History</a></p>
+<p><a href="news.php">News or some snippets</a></p>
+<p><a href="price-history.php">Price history</a></p>
+<p><a href="expansion-seasons.php">Expansion Seasons</a></p>
 </div>
 
 <div class="icat">
-<h2>Miscellaneous</h2>
-<a href="controls.php">Controls (keyboard etc)</a><br><br>
-<a href="mining.php">Mining</a><br><br>
-<a href="pristine-reserves-metallic-rings.php">Pristine Reserves Metallic Rings</a><br><br>
-<a href="crime.php">Crime (and punishment)</a><br><br>
-<a href="federation-empire-rank-progression.php">Federation and Empire rank progression</a><br><br>
-<a href="frame-rates-4k-resolution.php">Frame rates 4k resolution</a><br><br>
-<a href="journal.php">Journal</a>, players journal or commander's log<br><br>
-<a href="community-api.php">Community API</a><br><br>
-<a href="howto-install-elite-dangerous.php">HOWTO Install Elite Dangerous</a><br><br>
-<a href="multiple-accounts.php">Multiple Accounts</a><br><br>
-<a href="launcher.php">Launcher</a><br><br>
-<a href="guardian-fsd-booster.php">Guardian FSD Booster</a>
+	<h2>Miscellaneous</h2>
+
+<p><a href="controls.php">Controls (keyboard etc)</a></p>
+<p><a href="mining.php">Mining</a></p>
+<p><a href="pristine-reserves-metallic-rings.php">Pristine Reserves Metallic Rings</a></p>
+<p><a href="crime.php">Crime (and punishment)</a></p>
+<p><a href="federation-empire-rank-progression.php">Federation and Empire rank progression</a></p>
+<p><a href="frame-rates-4k-resolution.php">Frame rates 4k resolution</a></p>
+<p><a href="journal.php">Journal</a>, players journal or commander's log</p>
+<p><a href="community-api.php">Community API</a></p>
+<p><a href="howto-install-elite-dangerous.php">HOWTO Install Elite Dangerous</a></p>
+<p><a href="multiple-accounts.php">Multiple Accounts</a></p>
+<p><a href="launcher.php">Launcher</a></p>
+<p><a href="guardian-fsd-booster.php">Guardian FSD Booster</a></p>
 </div>
 
 <div class="icat">
-<h2>Equipment</h2>
-<a href="ships.php">Ships</a><br><br>
-<a href="space-stations.php">Space Stations</a><br><br>
-<a href="edshipyard.php">edshipyard</a><br><br>
-<a href="engineers.php">Engineers</a>
+	<h2>Equipment</h2>
+
+<p><a href="ships.php">Ships</a></p>
+<p><a href="space-stations.php">Space Stations</a></p>
+<p><a href="edshipyard.php">edshipyard</a></p>
+<p><a href="engineers.php">Engineers</a></p>
 </div>
 
 <div class="icat">
-<h2>PMC CMDR's Log</h2>
-<a href="my-path-to-anaconda.php">My path to Anaconda</a><br><br>
-<a href="cmdr-snake-man-my-path-to-a-triple-elite.php">My path to a Triple Elite</a><br><br>
-<a href="https://www.youtube.com/user/pmcsince1984">PMC Elite Dangerous Youtube videos</a><br><br>
-<a href="pmc-galaxy-loop.php">PMC Galaxy Loop</a><br><br>
-<a href="from-scratch-trading.php">From Scratch Trading (in open play)</a>
+	<h2>PMC CMDR's Log</h2>
+
+<p><a href="my-path-to-anaconda.php">My path to Anaconda</a></p>
+<p><a href="cmdr-snake-man-my-path-to-a-triple-elite.php">My path to a Triple Elite</a></p>
+<p><a href="https://www.youtube.com/user/pmcsince1984">PMC Elite Dangerous Youtube videos</a></p>
+<p><a href="pmc-galaxy-loop.php">PMC Galaxy Loop</a></p>
+<p><a href="from-scratch-trading.php">From Scratch Trading (in open play)</a></p>
 </div>
 
 </section>
 
 <footer>
-<br><br>
 <p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
 <?php include("include/w3-validator-logo.php"); ?>
 </footer>
