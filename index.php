cca3382e489c47db6e735a9a894ba486295fe25e
Snake Man <snakeman@phnet.fi>
2022-01-11 10:42:05 +0000

    2nd CMDR LTD mining story page added

--- index.php
+++ index.php
@@ -110,13 +110,14 @@ These pages are just <b>CMDR Snake Man's personal notes</b>, written since start
 <div class="icat">
 	<h2>PMC CMDR's Log</h2>
 
-<p><a href="my-path-to-anaconda.php">My path to Anaconda</a></p>
 <p><a href="cmdr-snake-man-my-path-to-a-triple-elite.php">My path to a Triple Elite</a></p>
-<p><a href="https://www.youtube.com/playlist?list=PLKt0kt4BAefkf_dCsdlj3nSrvgwA0KssM" target="_blank">PMC Elite Dangerous Youtube videos</a></p>
-<p><a href="pmc-galaxy-loop.php">PMC Galaxy Loop</a></p>
-<p><a href="from-scratch-trading.php">From Scratch Trading (in open play)</a></p>
-<p><a href="exploration-pmc-earth-like-world-tour.php">Exploration PMC Earth Like World Tour</a></p>
 <p><a href="exploration-mining-trading-2019.php">Exploration, Mining, Trading 2019</a></p>
+<p><a href="exploration-pmc-earth-like-world-tour.php">Exploration PMC Earth Like World Tour</a></p>
+<p><a href="from-scratch-trading.php">From Scratch Trading (in open play)</a></p>
+<p><a href="https://www.youtube.com/playlist?list=PLKt0kt4BAefkf_dCsdlj3nSrvgwA0KssM" target="_blank">PMC Elite Dangerous Youtube videos</a></p>
+<p><a href="my-path-to-anaconda.php">My path to Anaconda</a></p>
+<p><a href="pmc-galaxy-loop.php">PMC Galaxy Loop</a></p>
+<p><a href="second-cmdr-mining-low-temperature-diamonds.php">Second CMDR Mining Low Temperature Diamonds</a></p>
 </div>
 
 </section>
