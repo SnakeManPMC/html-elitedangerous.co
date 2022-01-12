846f4ac3df1327ea78915db74ef49af595579561
Snake Man <snakeman@phnet.fi>
2022-01-12 12:50:51 +0000

    Wanderer voyage home page added

--- index.php
+++ index.php
@@ -117,6 +117,7 @@ These pages are just <b>CMDR Snake Man's personal notes</b>, written since start
 <p><a href="https://www.youtube.com/playlist?list=PLKt0kt4BAefkf_dCsdlj3nSrvgwA0KssM" target="_blank">PMC Elite Dangerous Youtube videos</a></p>
 <p><a href="my-path-to-anaconda.php">My path to Anaconda</a></p>
 <p><a href="pmc-galaxy-loop.php">PMC Galaxy Loop</a></p>
+<p><a href="pmc-wanderer-voyage-home.php">PMC Wanderer Voyage Home</a></p>
 <p><a href="second-cmdr-mining-low-temperature-diamonds.php">Second CMDR Mining Low Temperature Diamonds</a></p>
 </div>
 
