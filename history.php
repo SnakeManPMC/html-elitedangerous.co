48c4a27d863285f407bd534ef02e65fef5d36026
Snake Man <snakeman@phnet.fi>
2024-03-07 13:57:28 +0000

    Odyssey update 18.01 history added

--- history.php
+++ history.php
@@ -299,6 +299,7 @@ Tuesday. Aug 29th, 2023. Odyssey (Update 16.01)<br>
 Monday. Oct 16th, 2023. Odyssey (Update 17)<br>
 Tuesday. Dec 12th, 2023. Odyssey (Update 17.01)<br>
 Monday. Feb 26th, 2024. Odyssey (Update 18)<br>
+Thursday. Mar 7th, 2024. Odyssey (Update 18.01)<br>
 </p>
 
 <h2>Detailed major release times (UTC + 2hrs or 3hrs daylight lameness time):</h2>
@@ -1120,6 +1121,10 @@ Odyssey (Update 17.01)<br>
 Odyssey (Update 18)<br>
 ?hrs game servers down.<br>
 ?hrs servers back up
+<br><br>
+Odyssey (Update 18.01)<br>
+0700hrs game servers down.<br>
+1000hrs servers back up
 </p>
 </section>
 
