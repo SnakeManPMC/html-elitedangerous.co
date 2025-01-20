a0821aaf12b6cd2772be3be059751763d94f730a
Snake Man <snakeman@phnet.fi>
2025-01-20 10:11:05 +0000

    description improved, footer.php added

--- eddn.php
+++ eddn.php
@@ -28,17 +28,23 @@
 
 <p>It is following the footsteps of Andreas Market Scraper and EDMN.</p>
 
+<p>
+Quote from EDDN github page: <i>Elite Dangerous Data Network is a tool that facilitates players of the game Elite Dangerous, including its expansions, sharing data about the game galaxy with others. By pooling data in a common format, tools and analyses can be produced that add an even greater depth and vibrancy to the in-game universe.</i>
+</p>
+
+<p>
+<i>EDDN is not run by or affiliated with the developer of the game - Frontier Developments.</i>
+</p>
+
+<p>
+<i>The live EDDN service itself does not store any data, and thus makes no archive or "current state" available to anyone. What it provides is a stream of live data to any interested parties. Some of those then make aggregated data available for general use.</i>
+</p>
+
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
