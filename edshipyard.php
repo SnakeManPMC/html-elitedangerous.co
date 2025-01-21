ab0c077301331b5ad0410cd571d619c3a20ab2de
Snake Man <snakeman@phnet.fi>
2025-01-21 09:38:08 +0000

    2025 update for edshipyard page

--- edshipyard.php
+++ edshipyard.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>PMC Elite Dangerous, ED Shipyard</title>
+<title>PMC Elite Dangerous ED Shipyard</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, ED Shipyard">
-<META name="keywords" content="PMC, Elite Dangerous, edshipyard">
+<META name="description" content="PMC Elite Dangerous ED Shipyard">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,34 +15,33 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous, ED Shipyard</b></h1>
+	<h1>PMC Elite Dangerous ED Shipyard</h1>
+
+<p>
+Elite Dangerous Shipyard (EDSY) website, helps you design your outfitting before going in-game outfitting menu. Very helpful tool to design your perfect ship outfitting setup.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
+	<h2>EDSY Website</h2>
 
-<p>ED Shipyard is a ship builder website by Taleden. You can design and try out all kinds of outfitting module variations for your ships. You can see jump ranges with laden or unlanden states etc. Don't buy ships or modules without trying out different combinations in ED Shipyard first.
+<p>Elite Dangerous Shipyard (EDSY) is a ship builder website by Taleden. You can design and try out all kinds of outfitting module variations for your ships. You can see jump ranges with laden or unlanden states etc. Don't buy ships or modules without trying out different combinations in ED Shipyard first.
 </p>
 
 <p>When you have chosen your favourite ship and outfitting module configuration, you can search stations that are selling ships and modules from <a href="eddb.php">EDDB</a> very easily, no more flying around galaxy searching for high tech stations.
 </p>
 
 <p>
-<a href="https://edsy.org">ED Shipyard Homepage</a> <br><br>
+<a href="https://edsy.org" target="_blank">ED Shipyard Homepage</a>
 </p>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
