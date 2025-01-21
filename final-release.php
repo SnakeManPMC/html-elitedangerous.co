0394a6f48dd8b67997c89810441c9cb9324cb511
Snake Man <snakeman@phnet.fi>
2025-01-21 10:29:57 +0000

    2025 update for final-release page

--- final-release.php
+++ final-release.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Final Release</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, Final Release">
+<META name="description" content="PMC Elite Dangerous 2014 Final Game Release">
 <META name="keywords" content="PMC, Elite Dangerous, Final Release">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,7 +16,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - Final Release</b></h1>
+	<h1>PMC Elite Dangerous - Final Release</h1>
+
+<p>
+Elite Dangerous 2014 Final Game Release. This obviously is not final release for the game forever, this was v1.0 final version release, the launch of the game, the big moment all Alpha backers and preview Beta testers had waited for a long time.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -47,13 +51,7 @@ Post Final Release News:<br>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
