97e724e959fb438ef6d62a8328eae362e5c1f08f
Snake Man <snakeman@phnet.fi>
2025-01-21 13:16:03 +0000

    2025 update for ltd mining story page

--- second-cmdr-mining-low-temperature-diamonds.php
+++ second-cmdr-mining-low-temperature-diamonds.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Second CMDR Mining Low Temperature Diamonds - PMC Elite Dangerous</title>
+<title>Second CMDR Mining LTD - PMC Elite Dangerous</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
 <META name="description" content="Second CMDR Mining Low Temperature Diamonds - PMC Elite Dangerous">
-<META name="keywords" content="Mining, Low, Temperature, Diamonds, LTD, 2020, PMC, Elite, Dangerous">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>Second CMDR Mining Low Temperature Diamonds PMC Elite Dangerous</b></h1>
+	<h1>Second CMDR Mining LTD PMC Elite Dangerous</h1>
+
+<p>
+It was 2020 summer when Low Temperature Diamonds (LTD) mining frenzy happened, you had to be there to experience it, nothing like it since.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -578,13 +581,7 @@ If you enjoyed this CMDR's log story, check out our others like:
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y"));?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
