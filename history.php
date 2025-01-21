8dee8b75ee522f4a5834c7e94d033c4cdcf3c41f
Snake Man <snakeman@phnet.fi>
2025-01-21 10:49:53 +0000

    2025 update for history page

--- history.php
+++ history.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - History of Releases</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC Elite Dangerous, History of Releases">
+<META name="description" content="PMC Elite Dangerous History of Releases">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - History of Releases</b></h1>
+	<h1>PMC Elite Dangerous - History of Releases</h1>
+
+<p>
+History of all Elite Dangerous game releases, server updates, etc with time stamps and version numbers.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -1194,13 +1198,7 @@ Odyssey Update 19.03<br>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
