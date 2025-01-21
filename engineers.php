0f00e03b84a10536749cb9be770e55eb57a156c9
Snake Man <snakeman@phnet.fi>
2025-01-21 09:55:19 +0000

    2025 update for engineers page

--- engineers.php
+++ engineers.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - The Engineers</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, The Engineers">
-<META name="keywords" content="PMC, Elite Dangerous, Engineers">
+<META name="description" content="PMC Elite Dangerous The Engineers">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,9 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - The Engieers</b></h1>
+	<h1>PMC Elite Dangerous - The Engineers</h1>
 
-<p>Basic The Engineers info</p>
+<p>
+Basic The Engineers info. Engineering ships and outfitting modules. Basically it means you can tweak the performance of your ships, make lasers more powerful, ships more faster, stronger armor or hyper drive (FSD) to jump further out, etc.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -120,13 +121,7 @@ Strange wake solutions == Frameshift wake scanner
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
