b68631f7b77feededd36bf5fc2bfdd6e79a09518
Snake Man <snakeman@phnet.fi>
2025-01-21 08:54:19 +0000

    2025 update for commodities-market page

--- commodities-market.php
+++ commodities-market.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Commodities Market</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite, Dangerous, Commodities, Market">
-<META name="keywords" content="PMC, Elite, Dangerous, Commodities, Market">
+<META name="description" content="PMC Elite Dangerous Commodities Market">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - Commodities Market</b></h1>
+	<h1>PMC Elite Dangerous - Commodities Market</h1>
+
+<p>
+Trading goods in Elite Dangerous commodities market, how to make money fast in Elite.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -26,7 +29,7 @@
 
 <h2>Trading Tips</h2>
 
-<p>Use EliteOCR to grab market data then export it to EDDN. Every station you visit, once a day (when you hammer a trade route, there is really no point of updating one station except once maybe twice a day). Register to http://eddb.io and fill in stations that are missing for landing pad size and distance to star data. Finally use TradeDangerous as trading tool to search for profitable trade routes. Ignore other sites/tools, they are waste of time.</p>
+<p>Use EliteOCR to grab market data then export it to EDDN. Every station you visit, once a day (when you hammer a trade route, there is really no point of updating one station except once maybe twice a day). Register to <a href="eddb.php">EDDB</a> and fill in stations that are missing for landing pad size and distance to star data. Finally use TradeDangerous as trading tool to search for profitable trade routes. Ignore other sites/tools, they are waste of time.</p>
 
 <p><b>1000cr budget for commanders who just bought the game:</b></p>
 
@@ -317,13 +320,7 @@ See also <a href="trading-week-2015-01-13.php">Elite Trading Week 2015-01-13</a>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
