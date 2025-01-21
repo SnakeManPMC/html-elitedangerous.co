2dc3abca830af812e6542d7673d09ce3684c1ce8
Snake Man <snakeman@phnet.fi>
2025-01-21 09:11:53 +0000

    2025 update for controls page

--- controls.php
+++ controls.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>PMC Elite Dangerous - Controls</title>
+<title>PMC Elite Dangerous - Keyboard Controls</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite, Dangerous, Controls">
-<META name="keywords" content="PMC, Elite, Dangerous, Controls">
+<META name="description" content="PMC Elite Dangerous Keyboard Controls">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,15 +15,17 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - Controls</b></h1>
+	<h1>PMC Elite Dangerous - Keyboard Controls</h1>
+
+<p>
+Keyboard controls and joystic Hands On Throttle And Stick (HOTAS) controls.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
-	<h2>Info about Controls like Keyboard of Joystick HOTAS etc.</h2>
-
-<h2>Keyboard Layouts</h2>
+	<h2>Keyboard Layouts</h2>
 
 <p>Keyboard Layouts are saved to:<br>
 Bindings are saved in %LOCALAPPDATA%\Frontier Developments\Elite Dangerous\Options\Bindings<br>
@@ -128,13 +129,7 @@ Elite v2.0 Horizons installs to edlauncher\Products\elite-dangerous-64\ director
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
