f41f859c9e58543602e705fb0b859b05ee41e580
Snake Man <snakeman@phnet.fi>
2025-01-21 09:43:16 +0000

    2025 update for edsm page

--- edsm.php
+++ edsm.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Star Map EDSM</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, EDSM">
-<META name="keywords" content="PMC, Elite Dangerous, EDSM">
+<META name="description" content="PMC Elite Dangerous Star Mar EDSM">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,14 +15,17 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous, Star Map (EDSM)</b></h1>
+	<h1>PMC Elite Dangerous Star Map</h1>
 
-<p>By AnthorNet</p>
+<p>
+Elite Dangerous Star Map (EDSM) website by AnthorNet. If you are explorer, this is a must use resource for you to plot and track your journey across the star systems of Milky Way galaxy.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
+	<h2>Elite Dangerous Star Map</h2>
 
 <p>
 Elite Dangerous Star Map (EDSM) is a community effort to store and calculate systems coordinates around the Elite Dangerous Galaxy.
@@ -35,20 +37,14 @@ Elite Dangerous Star Map (EDSM) is a community effort to store and calculate sys
 <p>Please note that elite star coordinator (edstarcoordinator.com) or EDSC is obsolete website and should not be used anymore, all your star system information should be stored to EDSM instead.
 </p>
 
-<p><a href="http://www.edsm.net/">EDSM Homepage</a>
+<p><a href="https://www.edsm.net/" target="_blank">EDSM Homepage</a>
 </p>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
