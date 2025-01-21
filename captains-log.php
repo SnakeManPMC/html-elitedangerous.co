50a38280280b20845618725963d0905aedcbdb70
Snake Man <snakeman@phnet.fi>
2025-01-21 08:37:21 +0000

    2025 update for captains-log page

--- captains-log.php
+++ captains-log.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous Captain's Log</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
 <META name="description" content="PMC, Elite Dangerous, Captain's Log">
-<META name="keywords" content="PMC, Elite Dangerous, Captain's Log">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous Captain's Log</b></h1>
+	<h1>PMC Elite Dangerous Captain's Log</h1>
+
+<p>
+Commanders log tool called Captain's Log by Genar_Hofoen.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -28,26 +31,20 @@
 </p>
 
 <p>
-Genar_Hofoen made a Frontier Forums <a href="https://forums.frontier.co.uk/showthread.php/155545-Explorer-s-Application-Captain-s-Log?p=4679492&viewfull=1#post4679492">Captain's Log 2 Beta Announcement</a> at the day of Elite v2.2 launch. Check out <a href="http://scarygliders.net/captains-log/">Official Captain's Log Homepage</a>.
+Genar_Hofoen made a Frontier Forums Captain's Log 2 Beta Announcement at the day of Elite v2.2 launch. Check out Official Captain's Log Homepage. Edit: 2025-01-21T08:30:49Z, frontier forum topic and homepage links dead. Quick search finds new forum topic link: <a href="https://forums.frontier.co.uk/threads/explorers-application-captains-log.155545/" target="_blank">FDEV forum "[Explorer's Application] Captain's Log"</a>.
 </p>
 
 <p>Uses SQLite3 database to store your travels and notes. Has very nice graphical percentage circle (pie?) presentation of scanned planet materials and it also shows icons for J1, J2 and J3 which are Elite explorers slang term for "Jumponium" on basic, standard and premium boost levels. Very easy and nice way to see what materials current systems scanned planets provide.
 </p>
 
-<p>Note: <a href="https://captainslog.scarygliders.net/download/captains-log-2x/">Captain's Log v2.1.15</a> is still not working properly in 4k desktop resolutions, some of the text is cut off due some user interface scale issues. You can use Captain's Log in 4k, but its... well annoying issue to say the least.
+<p>Note: Captain's Log v2.1.15 is still not working properly in 4k desktop resolutions, some of the text is cut off due some user interface scale issues. You can use Captain's Log in 4k, but its... well annoying issue to say the least.
 </p>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
