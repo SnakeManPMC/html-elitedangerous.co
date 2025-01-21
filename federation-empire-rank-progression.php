bcf43a373eb5f7ab02886ab4d664e619b39e2ab6
Snake Man <snakeman@phnet.fi>
2025-01-21 10:25:28 +0000

    2025 update for fed emp rank prog page

--- federation-empire-rank-progression.php
+++ federation-empire-rank-progression.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Federation and Empire Rank Progression</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, Federation and Empire Rank Progression">
-<META name="keywords" content="PMC, Elite Dangerous, Federation, Empire, Rank, Progression">
+<META name="description" content="PMC Elite Dangerous Federation and Empire Rank Progression">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,10 +15,10 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous Federation and Empire Rank Progression</b></h1>
+	<h1>PMC Elite Dangerous Federation and Empire Rank Progression</h1>
 
 <p>
-How to get them ranks
+How to get them ranks.
 </p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
@@ -43,13 +42,7 @@ PMCTODO
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
