d25f00ff74ef8852211de29ea0f3c8f905b46704
Snake Man <snakeman@phnet.fi>
2025-01-21 10:53:23 +0000

    2025 update for howto install page

--- howto-install-elite-dangerous.php
+++ howto-install-elite-dangerous.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - HOWTO Install Elite Dangerous</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, HOWTO Install Elite Dangerous">
-<META name="keywords" content="PMC, Elite Dangerous, HOWTO, Install">
+<META name="description" content="PMC Elite Dangerous HOWTO Install Elite Dangerous">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,10 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - HOWTO Install</b></h1>
+	<h1>PMC Elite Dangerous - HOWTO Install</h1>
 
-<p>Install Instructions</p>
-
+<p>
+Install Instructions. How to get Elite Dangerous game installed to your computer.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -27,7 +27,7 @@
 <?php include("/var/www/include/section-start.php"); ?>
 	<h2>HOWTO Install Elite Dangerous</h2>
 
-<p>First you must purchase the game by registering account into <a href="https://www.frontierstore.net/">frontier store</a>, you'll find <a href="https://www.frontierstore.net/games/elite-dangerous-cat.html">Elite Dangerous</a> category in there.
+<p>First you must purchase the game by registering account into <a href="https://www.frontierstore.net/" target="_blank">frontier store</a>, you'll find <a href="https://www.frontierstore.net/games/elite-dangerous-cat.html" target="_blank">Elite Dangerous</a> category in there.
 </p>
 
 <p>Then you purchase the game. After the purchasing process is done you'll get the game title to show up in My Products page.
@@ -36,7 +36,7 @@
 <p>Click and download the elite client installer exe from My Products page. This is the installer that is used to, well install the game. The installer is <i>not</i> the game, you use the installer to download the game from internet.
 </p>
 
-<p>If you have trouble finding the installer link, here is the direct link to <a href="http://hosting.zaonce.net/elite/Client-Installer.exe">hosting.zaonce.net/elite/Client-Installer.exe</a> file.
+<p>If you have trouble finding the installer link, here is the direct link to <a href="http://hosting.zaonce.net/elite/Client-Installer.exe" target="_blank">hosting.zaonce.net/elite/Client-Installer.exe</a> file.
 </p>
 
 <p>Run the installer and point to some proper directory (not c:\program files\ oh no) and wait, depending on how busy frontier servers are and what is your internet connection speed (and location), it will take a while for the download and install to complete.
@@ -52,13 +52,7 @@
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y"));?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
