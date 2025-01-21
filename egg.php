1e3143e897886479629f070ee22d7214dd359903
Snake Man <snakeman@phnet.fi>
2025-01-21 09:47:13 +0000

    2025 update for egg page

--- egg.php
+++ egg.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>Mining The Egg - PMC Elite Dangerous</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="Mining The Egg, PMC Elite Dangerous">
+<META name="description" content="Mining The Egg PMC Elite Dangerous">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -17,7 +17,9 @@
 <?php include("/var/www/include/header-start.php"); ?>
 	<h1>PMC Elite Dangerous Mining - The EGG</h1>
 
-<p>What is the egg?</p>
+<p>
+What is the egg?
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -158,13 +160,7 @@ See also <a href="mining.php">PMC Elite Dangerous: Mining</a>.
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y"));?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
