300e41b2fbc258099ab9e55ef4056f1eecdfe828
Snake Man <snakeman@phnet.fi>
2025-01-21 13:24:54 +0000

    2025 update for space-stations page

--- space-stations.php
+++ space-stations.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Space Stations</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite, Dangerous, Space Stations">
-<META name="keywords" content="PMC, Elite, Dangerous, Space Stations">
+<META name="description" content="PMC Elite Dangerous Space Stations">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,14 +15,16 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - Space Stations</b></h1>
+	<h1>PMC Elite Dangerous - Space Stations</h1>
+
+<p>
+List of all the space stations available.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
-	<h2>List of all the Space Stations available.</h2>
-
 <h2>Coriolis</h2>
 
 <img src="images/system-map-coriolis-station.jpg" alt="System Map Coriolis Station" height="402" width="390">
@@ -62,13 +63,7 @@
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
