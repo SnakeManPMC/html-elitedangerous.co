c2ec830348cfe4334fb53c8c47e5da67e63996ff
Snake Man <snakeman@phnet.fi>
2025-01-21 12:03:03 +0000

    2025 update for maddavos page

--- maddavos-market-share.php
+++ maddavos-market-share.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Maddavo's Market Share</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, Maddavo's Market Share">
-<META name="keywords" content="PMC, Elite Dangerous, Maddavo, Market, Share, MMS">
+<META name="description" content="PMC Elite Dangerous Maddavo's Market Share">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,9 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - Maddavo's Market Share</b></h1>
+	<h1>PMC Elite Dangerous - Maddavo's Market Share</h1>
 
-<p>Maddavo's Market Share (MMS)</p>
+<p>
+Maddavo's Market Share (MMS). Commodity market trading data tool. Now obsolete in 2025-01-21T12:01:12Z.
+</p>
 
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
@@ -26,6 +27,10 @@
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
 	<h2>Maddavo's Market Share</h2>
+
+<p>
+2025-01-21T12:01:30Z this tool is now obsolete, this web page is up just for historical nostalgia reasons.
+</p>
 
 <p>Maddavo's Market Share is the main data hub for <a href="tradedangerous.php">Trade Dangerous</a> (TD) trading tool.
 </p>
