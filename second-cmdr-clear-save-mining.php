ea4efd05ac759851d647b87c46175dbd9f7c6981
Snake Man <snakeman@phnet.fi>
2025-01-21 13:12:08 +0000

    2025 update for 2nd cmdr ltd mining story page

--- second-cmdr-clear-save-mining.php
+++ second-cmdr-clear-save-mining.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Second CMDR Clear Save Mining (2020 LTD era) - PMC Elite Dangerous</title>
+<title>Second CMDR Clear Save Mining - PMC Elite Dangerous</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
 <META name="description" content="Second CMDR Clear Save Mining (2020 LTD era) - PMC Elite Dangerous">
-<META name="keywords" content="Mining, Temperature, Diamonds, LTD, 2020, Fleet, Carrier, PMC, Elite, Dangerous">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>Second CMDR Clear Save Mining (2020 LTD era) - PMC Elite Dangerous</b></h1>
+	<h1>Second CMDR Clear Save Mining - PMC Elite Dangerous</h1>
+
+<p>
+This is the famous 2020 Low Temperature Diamond (LTD) era mining frenzy space exploration story about CMDR Snake Man (me) mining LTDs for billions of credits.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -2642,13 +2645,7 @@ If you enjoyed this CMDR's log story, check out our others like:
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y"));?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
