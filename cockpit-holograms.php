7bc55de60bd12b56391d87fa1b302505bbcc5b8f
Snake Man <snakeman@phnet.fi>
2025-01-21 08:46:01 +0000

    2025 update for cockpit-holograms page

--- cockpit-holograms.php
+++ cockpit-holograms.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Cockpit Holograms</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC Elite Dangerous, Cockpit Holograms">
-<META name="keywords" content="PMC, Elite Dangerous, Cockpit, Holograms">
+<META name="description" content="PMC Elite Dangerous, User Interface Cockpit Holograms">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous, Cockpit Holograms</b></h1>
+	<h1>PMC Elite Dangerous Cockpit Holograms</h1>
+
+<p>
+What are the user interface cockpit hologram images, how to properly use them, to understand their meaning.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -70,13 +73,7 @@
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
