ec41198bdbbb0a7af4f4f9c3c1df28f902964b35
Snake Man <snakeman@phnet.fi>
2025-01-21 10:46:16 +0000

    2025 update for guardian-fsd-booster page

--- guardian-fsd-booster.php
+++ guardian-fsd-booster.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Guardian FSD Booster</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC Elite Dangerous, Guardian FSD Booster">
-<META name="keywords" content="PMC, Elite Dangerous, FSD, Guardian, Booster">
+<META name="description" content="PMC Elite Dangerous Guardian FSD Booster">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous, Guardian FSD Booster</b></h1>
+	<h1>PMC Elite Dangerous Guardian FSD Booster</h1>
+
+<p>
+HOWTO grind your way into Guarding Frame Shift Drive (FSD) Booster to increase ships hyper space jump range.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -118,13 +121,7 @@ Once you arrive to the station go to starport services -> contacts -> technology
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y"));?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
