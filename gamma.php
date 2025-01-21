7649f679c31e012645cc773b0d103a943885fa57
Snake Man <snakeman@phnet.fi>
2025-01-21 10:43:25 +0000

    2025 update for gamma page

--- gamma.php
+++ gamma.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Gamma</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, Gamma">
-<META name="keywords" content="PMC, Elite Dangerous, Gamma">
+<META name="description" content="PMC Elite Dangerous Gamma">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - Gamma</b></h1>
+	<h1>PMC Elite Dangerous - Gamma</h1>
+
+<p>
+Early 2014 Elite Dangerous game development when it was heading for the Gamma release version.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -38,13 +41,7 @@ Gamma or pre-release "Preview" build on November 22nd, 2014.<br>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
