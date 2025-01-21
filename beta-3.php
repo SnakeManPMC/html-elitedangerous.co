6bdd8068804eaf64a086820e0a56b02e17108536
Snake Man <snakeman@phnet.fi>
2025-01-21 08:25:09 +0000

    2025 update for beta-3 page

--- beta-3.php
+++ beta-3.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Beta 3</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
 <META name="description" content="PMC, Elite Dangerous, Beta 3">
-<META name="keywords" content="PMC, Elite Dangerous, Beta 3">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -17,6 +16,10 @@
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
 	<h1>PMC Elite Dangerous - Beta 3</h1>
+
+<p>
+History page for old Beta 3 era of Elite Dangerous game testing on 2014.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -71,13 +74,7 @@ Beta 3.9 News:<br>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
