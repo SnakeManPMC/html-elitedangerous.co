2577d9b4eb551352be68b2399ee60035d013d796
Snake Man <snakeman@phnet.fi>
2025-01-21 08:28:47 +0000

    2025 update for bounty-hunting page

--- bounty-hunting.php
+++ bounty-hunting.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Bounty Hunting</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
 <META name="description" content="PMC, Elite Dangerous, Bounty Hunting">
-<META name="keywords" content="PMC, Elite Dangerous, Bounty Hunting">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - Bounty Hunting</b></h1>
+	<h1>PMC Elite Dangerous - Bounty Hunting</h1>
+
+<p>
+Bounty hunting. Searching and destroying wanted criminal ships for the bounty credits/money.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -93,13 +96,7 @@ Open galaxy map, select powerplay -> a lavigny-duval -> control (systems). Now y
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
