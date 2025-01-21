5a3264d357d3bc88d63bd09f829f353e6979ea2f
Snake Man <snakeman@phnet.fi>
2025-01-21 10:22:43 +0000

    2025 update for exploration special planets

--- exploration-special-planets.php
+++ exploration-special-planets.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>PMC Elite Dangerous, Exploration Special Planets</title>
+<title>PMC Elite Dangerous Exploration Special Planets</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC Elite Dangerous, Exploration Special Planets like smallest and largest planets">
-<META name="keywords" content="PMC, Elite Dangerous, Exploration, Planets">
+<META name="description" content="PMC Elite Dangerous Exploration Special Planets like smallest and largest planets">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous, Exploration Special Planets</b></h1>
+	<h1>PMC Elite Dangerous Exploration Special Planets</h1>
+
+<p>
+Elite Dangerous Exploration Special Planets like smallest and largest planets.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -106,13 +109,7 @@ Largest:<br>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
