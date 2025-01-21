cdb115863a1def358ff06699fa58a24b4a22e166
Snake Man <snakeman@phnet.fi>
2025-01-21 13:33:04 +0000

    2025 update for super cruise page

--- super-cruise.php
+++ super-cruise.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Super Cruise</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite, Dangerous, Super Cruise">
-<META name="keywords" content="PMC, Elite, Dangerous, Super Cruise">
+<META name="description" content="PMC Elite Dangerous Super Cruise">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - Super Cruise</b></h1>
+	<h1>PMC Elite Dangerous - Super Cruise</h1>
+
+<p>
+Super cruise is a scifi scheme to make you move really fast within a star system so you don't have to use time acceleration which doesn't work in multiplayer game.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -44,13 +47,7 @@ Blue Zone<br><img src="images/Throttle-Indicator-Blue-Zone.png" alt="Cockpit Thr
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
