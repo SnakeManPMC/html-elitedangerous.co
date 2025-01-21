7269f92c6ebce98bace6fa4f2a21c16f086035fd
Snake Man <snakeman@phnet.fi>
2025-01-21 12:43:31 +0000

    2025 update for pmc-most-wanted page

--- pmc-most-wanted.php
+++ pmc-most-wanted.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - PMC Most Wanted</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous - PMC Most Wanted">
-<META name="keywords" content="PMC, Elite Dangerous, Bounty, Most, Wanted">
+<META name="description" content="PMC Elite Dangerous - PMC Most Wanted">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - PMC Most Wanted</b></h1>
+	<h1>PMC Elite Dangerous - PMC Most Wanted</h1>
+
+<p>
+Bounty for most wanted CMDRs.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -25,20 +28,14 @@
 	<h2>PMC Most Wanted</h2>
 
 <p>
-<b>2020-06-06</b> This page was deleted as obsolete, sorry about that.
+<b>2020-06-06</b> This page was deleted as obsolete, sorry about that. You might be interested in <a href="bounty-hunting.php">bounty hunting</a> page.
 </p>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
