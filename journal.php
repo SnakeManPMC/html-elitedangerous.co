870f0716497cb66078adf51abc5bf4f78cbee500
Snake Man <snakeman@phnet.fi>
2025-01-21 11:12:22 +0000

    2025 update for journal page

--- journal.php
+++ journal.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>PMC Elite Dangerous Journal</title>
+<title>PMC Elite Dangerous Commanders Journal</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, Journal">
-<META name="keywords" content="PMC, Elite Dangerous, Journal">
+<META name="description" content="PMC Elite Dangerous Commanders Journal">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous Journal</b></h1>
+	<h1>PMC Elite Dangerous Commanders Journal</h1>
+
+<p>
+Commanders journal is a JSON file located in games users dir, this file logs all kinds of commodity market, space station, exploration, star system etc data. This file is used by community created tools to improve Elite Dangerous experience.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -25,7 +28,7 @@
 	<h2>Player Journal or Commander's Log</h2>
 
 <p>
-Frontier created <a href="https://forums.frontier.co.uk/showthread.php/275151-Commanders-log-manual-and-data-sample">Commanders log manual and data sample</a> topic where they introduce player journal or commander's log file. Elite client writes all kinds of cool data into this JSON file for 3rd party developers to read with their own clients.
+Frontier created <a href="https://forums.frontier.co.uk/threads/commanders-log-manual-and-data-sample.275151/" target="_blank">Commanders log manual and data sample</a> topic where they introduce player journal or commander's log file. Elite client writes all kinds of cool data into this JSON file for 3rd party developers to read with their own clients.
 </p>
 
 <p>Elite journal files are located in unfortunate "C:\Users\YOURNAME\Saved Games\Frontier Developments\Elite Dangerous\" directory. File names are Journal.DATE.01.log
@@ -35,13 +38,7 @@ Frontier created <a href="https://forums.frontier.co.uk/showthread.php/275151-Co
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
