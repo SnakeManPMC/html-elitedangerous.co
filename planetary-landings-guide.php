eff7db868d6d43357d7d36361369ea554f198ab7
Snake Man <snakeman@phnet.fi>
2025-01-21 12:24:52 +0000

    2025 update for planetary landings guide page

--- planetary-landings-guide.php
+++ planetary-landings-guide.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - Planetary Landings Guide</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
 <META name="description" content="PMC Elite Dangerous Planetary Landings Guide">
-<META name="keywords" content="PMC, Elite Dangerous, Planetary, Landings, Guide">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous, Planetary Landings Guide</b></h1>
+	<h1>PMC Elite Dangerous Planetary Landings Guide</h1>
+
+<p>
+Basic tutorial for Elite Dangerous planetary landings, how its done, what to avoid etc.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -202,13 +205,7 @@ PMCTODO https://www.youtube.com/watch?v=O2uYM2ykS1I
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
