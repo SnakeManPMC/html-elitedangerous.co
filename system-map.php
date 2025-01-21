8d3b476f0971b16a081d85d39729a80e3ad32e82
Snake Man <snakeman@phnet.fi>
2025-01-21 13:37:27 +0000

    2025 update for system map page

--- system-map.php
+++ system-map.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - System Map</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, System Map">
-<META name="keywords" content="PMC, Elite Dangerous, System Map, Exploration">
+<META name="description" content="PMC Elite Dangerous System Map">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,15 +15,21 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous - System Map</b></h1>
+	<h1>PMC Elite Dangerous - System Map</h1>
+
+<p>
+System map, meaning map of the star system you're currently at or viewing from galaxy map.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
-	<h2>On system map distance measurements 1 AU is 500Ls</h2>
-
 <h2>Planets</h2>
+
+<p>
+On system map distance measurements 1 AU is 500Ls.
+</p>
 
 <p><b>High Metal Content Planets, candidates for terraforming</b>. These are more valuable planets than normal high metal content ones.</p>
 
@@ -93,13 +98,7 @@
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
