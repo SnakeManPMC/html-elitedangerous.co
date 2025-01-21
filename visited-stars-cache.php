12f042b3a3a6ed3da754e2d2b7c39087eeb08de4
Snake Man <snakeman@phnet.fi>
2025-01-21 13:54:44 +0000

    2025 update for visited stars cache page

--- visited-stars-cache.php
+++ visited-stars-cache.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous - The Visited Stars Cache</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, The Visited Stars Cache">
-<META name="keywords" content="PMC, Elite Dangerous, Visited, Stars, Cache">
+<META name="description" content="PMC Elite Dangerous The Visited Stars Cache">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous The Visited Stars Cache</b></h1>
+	<h1>PMC Elite Dangerous The Visited Stars Cache</h1>
+
+<p>
+The Visited Stars Cache is a local files that flag star systems visited, its excellent way to keep track which systems have you already been to. Please understand that if you re-install Elite Dangerous game or whole Windows operating system, then you lose visited stars cache. Only way to avoid that is to backup the files.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -25,10 +28,10 @@
 	<h2>The Visited Stars Cache</h2>
 
 <p>
-2022-04-28T20:00:00Z Below text contains possibly outdated information, please do not contact Frontier Support about "its not working". See the forums.frontier.co.uk topic link for details.
+2022-04-28T20:00:00Z Below text contains possibly outdated information, please do not contact Frontier Support about <i>"its not working"</i>. See the forums.frontier.co.uk topic link for details.
 </p>
 
-<p>On v2.2 we got visited stars cache feature which lists all the visited star systems from your CMDR locally in your Elite client. See details from <a href="https://forums.frontier.co.uk/showthread.php/291643-The-Visited-Stars-cache">Frontier Forums The Visited Stars cache</a> topic.
+<p>On v2.2 we got visited stars cache feature which lists all the visited star systems from your CMDR locally in your Elite client. See details from <a href="https://forums.frontier.co.uk/showthread.php/291643-The-Visited-Stars-cache" target="_blank">Frontier Forums The Visited Stars cache</a> topic.
 </p>
 
 <p>The location of the file in your hard drive is:<br>
@@ -54,13 +57,7 @@ C:\Users\YOURNAME\AppData\Local\Frontier Developments\Elite Dangerous\NUMBERS-HE
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
