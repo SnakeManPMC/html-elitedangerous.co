30108d5aa49f826cf1a8c5dce94d7b117064abbb
Snake Man <snakeman@phnet.fi>
2025-01-21 09:05:58 +0000

    2025 update for conflict-zones page

--- conflict-zones.php
+++ conflict-zones.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>PMC Elite Dangerous, Conflict Zones</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, Conflict Zones">
-<META name="keywords" content="PMC, Elite Dangerous, Conflict, Zones">
+<META name="description" content="PMC Elite Dangerous Conflict Zones">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous, Conflict Zones</b></h1>
+	<h1>PMC Elite Dangerous Conflict Zones</h1>
+
+<p>
+Space ship combat in special conflict zones usually near a planet or some other important location.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -59,13 +62,7 @@ Also, bounties and combat bonds are given a slight passive boost in systems cont
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
