63764f51835b4b24df166c2cab3c90ed83ccc069
Snake Man <snakeman@phnet.fi>
2025-01-21 09:19:11 +0000

    2025 update for eddb page

--- eddb.php
+++ eddb.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>PMC Elite Dangerous, Elite Dangerous Data Base</title>
+<title>PMC Elite Dangerous Data Base</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="PMC, Elite Dangerous, EDDB">
-<META name="keywords" content="PMC, Elite Dangerous, EDDB">
+<META name="description" content="PMC Elite Dangerous EDDB">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +15,11 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1><b>PMC Elite Dangerous, EDDB</b></h1>
+	<h1>PMC Elite Dangerous EDDB</h1>
+
+<p>
+Elite Dangerous Data Base (EDDB) was a website ran by themroc, it was taken down in April 2023.
+</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
@@ -24,9 +27,9 @@
 <?php include("/var/www/include/section-start.php"); ?>
 	<h2>Elite Dangerous Data Base</h2>
 
-<p>EDDB is created by themroc and you can find it here; <a href="http://eddb.io">EDDB Homepage</a>.</p>
+<p>EDDB is created by themroc and you can find it here; <a href="https://eddb.io">EDDB Homepage</a>.</p>
 
-<p>Star system coordinates; <a href="http://starchart.club">Starchart</a> coords overrule EDDB coords.</p>
+<p>Star system coordinates; <a href="https://starchart.club">Starchart</a> coords overrule EDDB coords.</p>
 
 <p>EDDB now includes shipyard ships for sale and modules outfitting search database.</p>
 
@@ -34,13 +37,7 @@
 </section>
 
 <footer>
-<?php include("/var/www/include/footer-start.php"); ?>
-<p>Back to <a href="index.php">Elite Dangerous root page</a></p>
-<br><br>
-<p><i>PMC Elite Dangerous 2014 - <?php print(date("Y")); ?>.</i></p>
-<?php include("/var/www/include/footer-end.php"); ?>
-<?php include("/var/www/include/support.php"); ?>
-<?php include("/var/www/include/w3-validator-logo.php"); ?>
+<?php include("include/footer.php"); ?>
 </footer>
 
 </body>
