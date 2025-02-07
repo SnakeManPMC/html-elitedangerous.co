9992206c77db980dd09127f822bee89c384f650e
Snake Man <snakeman@phnet.fi>
2025-02-07 15:37:04 +0000

    stories time line sorted, years added

--- stories.php
+++ stories.php
@@ -28,19 +28,47 @@ Space Stories for Elite Dangerous written by Snake Man, PMC. These stories are s
 <?php include("/var/www/include/support.php"); ?>
 	<h2>Elite Space Stories</h2>
 
-<p><a href="stories-cmdr-snake-man-my-path-to-a-triple-elite.php">My path to a Triple Elite</a></p>
-<p><a href="stories-exploration-mining-trading-2019.php">Exploration, Mining, Trading 2019</a></p>
-<p><a href="stories-exploration-pmc-earth-like-world-tour.php">Exploration PMC Earth Like World Tour</a></p>
-<p><a href="stories-from-scratch-trading.php">From Scratch Trading (in open play)</a></p>
+<p>
+<b>2015</b>
+</p>
+<p><a href="trading-week-2015-01-13.php">2015-01-13 Elite Trading Week</a></p>
+<p><a href="stories-my-path-to-anaconda.php">2015-03-17 My path to Anaconda</a></p>
+<p><a href="stories-cmdr-snake-man-my-path-to-a-triple-elite.php">2015-03-27 My path to a Triple Elite</a></p>
+
+<p>
+<b>2016</b>
+</p>
+<p><a href="stories-pmc-galaxy-loop.php">2016-08-06 PMC Galaxy Loop</a></p>
+
+<p>
+<b>2018</b>
+</p>
+<p><a href="stories-from-scratch-trading.php">2018-11-13 From Scratch Trading (in open play)</a></p>
+<p><a href="stories-exploration-pmc-earth-like-world-tour.php">2018-12-07 Exploration PMC Earth Like World Tour</a></p>
+
+<p>
+<b>2019</b>
+</p>
+<p><a href="stories-exploration-mining-trading-2019.php">2019-09-08 Exploration, Mining, Trading 2019</a></p>
+
+<p>
+<b>2020</b>
+</p>
+<p><a href="second-cmdr-mining-low-temperature-diamonds.php">2020-06-14 Second CMDR Mining Low Temperature Diamonds</a></p>
+<p><a href="pmc-wanderer-voyage-home.php">2020-06-19 PMC Wanderer Voyage Home</a></p>
+<p><a href="second-cmdr-clear-save-mining.php">2020-06-29 Second CMDR Clear Save Mining (2020 LTD era)</a></p>
+
+<p>
+<b>2021</b>
+</p>
+<p><a href="searching-tritium-hotspots-2021.php">2021-03-21 Searching Tritium Hotspots 2021</a></p>
+<p><a href="pmc-wanderer-exploration-2021.php">2021-03-29 PMC Wanderer Exploration 2021</a></p>
+
+<p>
+Videos
+</p>
+
 <p><a href="https://www.youtube.com/playlist?list=PLKt0kt4BAefkf_dCsdlj3nSrvgwA0KssM" target="_blank">PMC Elite Dangerous Youtube videos</a></p>
-<p><a href="stories-my-path-to-anaconda.php">My path to Anaconda</a></p>
-<p><a href="stories-pmc-galaxy-loop.php">PMC Galaxy Loop</a></p>
-<p><a href="pmc-wanderer-exploration-2021.php">PMC Wanderer Exploration 2021</a></p>
-<p><a href="pmc-wanderer-voyage-home.php">PMC Wanderer Voyage Home</a></p>
-<p><a href="searching-tritium-hotspots-2021.php">Searching Tritium Hotspots 2021</a></p>
-<p><a href="second-cmdr-clear-save-mining.php">Second CMDR Clear Save Mining (2020 LTD era)</a></p>
-<p><a href="second-cmdr-mining-low-temperature-diamonds.php">Second CMDR Mining Low Temperature Diamonds</a></p>
-<p><a href="trading-week-2015-01-13.php">Elite Trading Week 2015-01-13</a></p>
 
 <?php include("include/description-elite.php"); ?>
 <?php include("include/description-stories.php"); ?>
